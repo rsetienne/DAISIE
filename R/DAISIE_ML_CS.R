0ac0c7f3afa750b22961401c329efd679e33057b
Neves-P <p.m.santos.neves@rug.nl>
2019-04-01 16:52:07 +0200

    fix island_ont label

--- R/DAISIE_ML_CS.R
+++ R/DAISIE_ML_CS.R
@@ -259,7 +259,7 @@ DAISIE_ML_CS <- DAISIE_ML <- function(
      res = 100,
      ddmodel = 0,
      cond = 0,
-     island_ontogeny = "const",
+     island_ontogeny = NA,
      eqmodel = 0,
      x_E = 0.95,
      x_I = 0.98,
@@ -274,7 +274,7 @@ DAISIE_ML_CS <- DAISIE_ML <- function(
 {
   if(datatype == 'single')
   {
-     if(island_ontogeny == "const")
+     if(is.na(island_ontogeny))
      {
        out = DAISIE_ML1(datalist,initparsopt,idparsopt,parsfix,idparsfix,idparsnoshift,res,ddmodel,cond,eqmodel,x_E,x_I,tol,maxiter,methode,optimmethod,CS_version,verbose,tolint, island_ontogeny)
      } else
