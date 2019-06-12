6a664994338361f03677052e03decb122bbb3b41
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-12 18:55:10 +0200

    Switch

--- R/is_simulation_outputs.R
+++ R/is_simulation_outputs.R
@@ -5,7 +5,11 @@
 #' @return TRUE of the input is a valid collection of simulation
 #' outputs.
 #' @author Richel J.C Bilderbeek, Pedro Neves
+#' @examples
+#' library(testthat)
+#'  
+#' expect_false(is_simulation_outputs("nonsense"))   
 #' @export
 is_simulation_outputs <- function(simulation_outputs) {
-     is.list(simulation_outputs)
+  is.list(simulation_outputs)
 }
---
+++ tests/testthat/test-is_simulation_outputs.R
@@ -0,0 +1,7 @@
+test_that("use", {
+  
+  expect_false(is_simulation_outputs("nonsense"))
+  expect_true(is_simulation_outputs(create_test_simulation_output()))
+
+
+})
