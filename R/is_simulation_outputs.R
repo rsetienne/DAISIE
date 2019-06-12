68ea549eebefd505335083eaf406affc6f978fec
Neves-P <p.m.santos.neves@rug.nl>
2019-06-12 18:49:43 +0200

    Switch

--- R/is_simulation_outputs.R
+++ R/is_simulation_outputs.R
@@ -1,11 +1,11 @@
 #' Measures if the input is a valid collection of simulation
 #' outputs.
-#' @param 
+#' @param simulation_outputs A list with matrices? of simulation produced by
+#' DAISIE_sim.  
 #' @return TRUE of the input is a valid collection of simulation
 #' outputs.
-#' @author Richel J.C Bilderbeek
+#' @author Richel J.C Bilderbeek, Pedro Neves
 #' @export
 is_simulation_outputs <- function(simulation_outputs) {
-  
-  
+     !is.list(simulation_outputs)
 }
