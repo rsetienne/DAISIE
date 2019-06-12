d1a734c69ef43ebc9317fe8d852143e4deef4692
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-12 18:45:14 +0200

    Switch

---
+++ R/is_simulation_outputs.R
@@ -0,0 +1,11 @@
+#' Measures if the input is a valid collection of simulation
+#' outputs.
+#' @param 
+#' @return TRUE of the input is a valid collection of simulation
+#' outputs.
+#' @author Richel J.C Bilderbeek
+#' @export
+is_simulation_outputs <- function(simulation_outputs) {
+  
+  
+}
