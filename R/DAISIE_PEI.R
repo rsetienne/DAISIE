14b2e0cff373ccfe962946893ebbecb1e3d28a03
Neves-P <p.m.santos.neves@rug.nl>
2019-06-14 13:54:41 +0200

    Silence example in convertprobdist

--- R/DAISIE_PEI.R
+++ R/DAISIE_PEI.R
@@ -124,16 +124,15 @@ DAISIE_probdist = function(pars1,pars2,tvec,initEI = c(0,0),initprobs = NULL)
 #' ### Compute the probability distribution at t = 4 and t = 8, for a mainland pool
 #' # size of 250 potential colonists and a vector of 5 parameters (cladogenesis, extinction,
 #' # clade-level carrying capacity, immigration, anagenesis) starting from an empty
-#' # island; output in list format
+#' # island; store in list format
 #' 
-#' pb = DAISIE_probdist(
+#' pb <- DAISIE_probdist(
 #'    pars1 = c(0.3,0.35,Inf,0.75,0.012),
 #'    pars2 = c(100,250),
 #'    tvec = c(4,8),
 #'    initEI = c(0,0),
 #'    initprobs = NULL
 #'    )
-#' DAISIE_convertprobdist(pb)
 #' 
 #' @export DAISIE_convertprobdist
 DAISIE_convertprobdist = function(pb)
