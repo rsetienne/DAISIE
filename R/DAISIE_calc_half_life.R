ac794321fa324e9234c1432c54cc2832c8bf1925
joshwlambert <j.w.l.lambert@rug.nl>
2019-08-04 13:22:49 +0200

    working on calc_half_life function and associated tests

--- R/DAISIE_calc_half_life.R
+++ R/DAISIE_calc_half_life.R
@@ -1,24 +1,30 @@
-calc_half_life <- function(stt_tables, pars) {
-  #initial number of species for each simulation
-  N0 <- matrix(nrow = length(stt_tables), ncol = 1)
-  for (i in 1:length(stt_tables)) {
-    N0[i, 1] <- sum(stt_tables[[i]][1, 2:4])
+DAISIE_calc_half_life <- function(island_replicates, pars) {
+  #calculate initial number of species for each simulation
+  N0 <- list()
+  for (i in 1:length(island_replicates)) {
+    N0[[i]] <- sum(island_replicates$stt_tables[[i]][1, 2:4])
   }
   #Half way between initial species diversity and K
-  spec_half <- matrix(nrow = length(stt_tables), ncol = 1)
+  spec_half <- list()
   for (i in 1:length(N0)) {
-    spec_half[i, 1] <- N0[i] - ( (N0[i] - pars[3]) / 2)
-    }
-  #get total number of species through time
-  total_spec_tables <- list()
-  total_spec <- list()
-  for (i in 1:length(stt_tables)) {
-    total_spec_tables[[i]] <- stt_tables[[i]][, 2:4]
-    total_spec[[i]] <- apply(X = total_spec_tables[[i]], MARGIN = 1, FUN = sum)
+    spec_half[[i]] <- N0[[i]] - ((N0[[i]] - pars[3]) / 2)
+    spec_half[[i]] <- round(spec_half[[i]], digits = 0)
   }
-  for (i in 1:length(stt_tables)) {
-    row_t_half <- list()
-    row_t_half[i] <- min(which(total_spec[[i]] == spec_half[i]))
+  #get total number of species through time
+  total_stt <- list()
+  for (i in 1:length(island_replicates)) {
+    total_stt[[i]] <- apply(island_replicates[[i]]$stt_table[, 2:4], 1, sum)
+  }
+  #the row on which species richness equals half way between N0 and K 
+  row_spec_half <- list()
+  for (i in 1:length(island_replicates)) {
+    row_spec_half[[i]] <- min(which(total_stt[[i]] == spec_half[[i]]))
+  }
+  #the time take to reach the half-life
+  half_life <- list()
+  for (i in 1:length(island_replicates)) {
+    half_life[[i]] <- as.numeric(island_replicates[[i]]$stt_table[1,1]) -
+      (island_replicates[[i]]$stt_table[[row_spec_half[[i]], 1]])
   }
   #if half-life has not been reached calculate using
   #Diamond 1972 exponential model
@@ -27,10 +33,32 @@ calc_half_life <- function(stt_tables, pars) {
     species_at_present <- sum(stt_tables[last_row, 2:4])
     half_life <- 1 / -log( (species_at_present / pars[3]) / (N0 - pars[3]))
   }
+  #calculate half-life from DAISIE_sim using a spline to compensate for sampling
+  total_spec <- matrix(ncol = 1, nrow = nrow(spec_through_time_tables[[1]]))
+  total_spec[, 1] <- sim[[1]][[1]]$stt_all[, 1]
+  total_spec <- cbind(total_spec, sum_spec_through_time)
+  #plot a spline for species diversity through time for each rep
+  time_seq <- seq(0, max(total_spec[, 1]), 0.001)
+  splines <- list()
+  half_life_predict <- list()
+  for (i in 2:ncol(total_spec)) {
+    splines[[i]] <- smooth.spline(x = total_spec[, 1],
+                                  y = total_spec[, i],
+                                  df = 10)
+    half_life_predict[[i]] <- predict(splines[[i]], time_seq)
+    half_life_predict[[i]] <- cbind(half_life_predict[[i]]$x,
+                                    half_life_predict[[i]]$y)
+  }
+  half_time <- matrix(ncol = 1, nrow = length(sim))
+  for (i in 1:ncol(sum_spec_through_time)) {
+    half_time[[i]] <- which.min(abs(sum_spec_through_time[, i] -
+                                      spec_half[i, 1]))
+    half_time[[i]] <- total_spec[half_time[i], 1]
+  }
   #the time take to reach the half-life
-  half_life <- matrix(nrow = length(stt_tables), ncol = 1)
-  for (i in 1:length(stt_tables)) {
-    half_life[i, 1] <- totaltime - (stt_tables[[row_t_half, 1]])
+  half_life <- matrix(nrow = length(sim), ncol = 1)
+  for (i in 1:length(sim)) {
+    half_life[i, 1] <- max(total_spec[, 1]) - (half_time[i])
   }
   return(half_life)
 }
---
+++ tests/testthat/test-DAISIE_calc_half_life.R
@@ -0,0 +1,121 @@
+context("DAISIE_calc_half_life")
+test_that("DAISIE_calc_half_life gives a valid half life", {
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  island_replicates <- DAISIE_sim_core(time = time,
+                                       mainland_n = mainland_n,
+                                       pars = pars)
+  half_life <- DAISIE_calc_half_life(island_replicates, pars)
+  expect_true(is.numeric(half_life))
+  expect_gt(half_life, 0)
+})
+
+test_that("DAISIE_calc_half_life gives a vector of half lives 
+          for oceanic IW simulation replicates", {
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  replicates <- 10
+  island_replicates <- list()
+  for (rep in 1:replicates) {
+    island_replicates[[rep]] <- DAISIE_sim_core(time = time,
+                                                mainland_n = mainland_n,
+                                                pars = pars)
+  }
+    half_life <- DAISIE_calc_half_life(island_replicates, pars)
+    expect_true(is.numeric(half_life))
+                expect_true(is.vector(half_life))
+                expect_length(half_life, replicates)
+})
+
+test_that("DAISIE_calc_half_life gives a vector of half lives
+          for oceanic CS simulation replicates", {
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  replicates <- 10
+  for (rep in 1:replicates) {
+    island_replicates[[rep]] <- list()
+    full_list = list()
+    for (m_spec in 1:mainland_n) {
+      full_list[[m_spec]] <- DAISIE_sim_core(
+        time = time,
+        mainland_n = 1,
+        pars = pars)
+      }
+    island_replicates[[rep]] <- full_list
+    }
+  half_life <- DAISIE_calc_half_life(island_replicates, pars)
+  expect_true(is.numeric(half_life))
+  expect_true(is.vector(half_life))
+  expect_length(half_life, replicates)
+})
+
+test_that("DAISIE_calc_half_life gives a vector of half lives
+          for non-oceanic IW simulation replicates", {
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  replicates <- 10
+  island_replicates <- list()
+  for (rep in 1:replicates) {
+    island_replicates[[rep]] <- DAISIE_sim_core(time = time,
+                                                mainland_n = mainland_n,
+                                                pars = pars,
+                                                island_type = "nonoceanic",
+                                                nonoceanic = c(0.1, 0.9))
+  }
+  half_life <- DAISIE_calc_half_life(island_replicates, pars)
+  expect_true(is.numeric(half_life))
+  expect_true(is.vector(half_life))
+  expect_length(half_life, replicates)
+})
+
+test_that("DAISIE_calc_half_life gives a vector of half lives
+          for non-oceanic CS simulation replicates", {
+  time <- 1
+  mainland <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  replicates <- 10
+  for (rep in 1:replicates) {
+    island_replicates[[rep]] <- list()
+    full_list = list()
+    for (m_spec in 1:mainland_n) {
+      full_list[[m_spec]] <- DAISIE_sim_core(
+        time = time,
+        mainland_n = 1,
+        pars = pars,
+        island_type = "nonoceanic",
+        nonoceanic = c(0.1, 0.9))
+    }
+    island_replicates[[rep]] <- full_list
+  }
+  half_life <- DAISIE_calc_half_life(island_replicates, pars)
+  expect_true(is.numeric(half_life))
+  expect_true(is.vector(half_life))
+  expect_length(half_life, replicates)
+})
+
+test_that("Half-life becomes shorter for diversity-dependent extinction", {
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  ddmodel <- c(1, 1, 1)
+  DD_sim <- DAISIE_sim_core(time = time,
+                            mainland_n = mainland_n,
+                            pars = pars,
+                            ddmodel = ddmodel)
+  time <- 1
+  mainland_n <- 10
+  pars <- c(1, 1, 20, 0.1, 1)
+  ddmodel <- c(1, 0, 1)
+  DI_sim <- DAISIE_sim_core(time = time,
+                            mainland_n = mainland_n,
+                            pars = pars,
+                            ddmodel = ddmodel)
+  DD_half_life <- DAISIE_calc_half_life(DD_sim, pars)
+  DI_half_life <- DAISIE_calc_half_life(DI_sim, pars)
+  expect_gt(DI_half_life, DD_half_life)
+  expect_true(DI_half_life != DD_half_life)							
+})
