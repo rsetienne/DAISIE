a89142b486c835739825cfcb9fd0118c847b3fa6
xieshu95 <xieshu95@gmail.com>
2019-08-13 22:02:48 +0200

    add parameters

--- tests/testthat/test-are_rates.R
+++ tests/testthat/test-are_rates.R
@@ -10,7 +10,13 @@ test_that("basic use", {
         clado_rate = 0.4,
         ext_rate_max = 0.5,
         immig_rate_max = 0.6,
-        clado_rate_max = 0.7
+        clado_rate_max = 0.7,
+        immig_rate1 = 0.1,
+        ext_rate1 = 0.2,
+        ana_rate1 = 0.3,
+        clado_rate1 = 0.4,
+        trans_rate = 0.5,
+        trans_rate1 = 0.5
       )
     )
   )
