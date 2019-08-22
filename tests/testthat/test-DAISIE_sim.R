c7858470d8e4491b82d927ed1c3fa2ca84f6f965
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-22 08:13:00 +0200

    Do not add vignette tests

--- tests/testthat/test-DAISIE_sim.R
+++ tests/testthat/test-DAISIE_sim.R
@@ -89,29 +89,4 @@ test_that("A keep last final state ontogeny run should produce no output and sto
   expect_true(is.matrix(out[[1]][[2]]$island_spec) || length(out[[1]][[2]]$branching_times) == 1)
 })
 
-test_that("vignette error", {
 
-  skip("Issue 70, Issue #70")
-
-  n_mainland_species <- 1000
-  island_age <- 4
-  n_replicates <- 10
-  set.seed(42)
-  clado_rate <- 2.550687345 # cladogenesis rate
-  ext_rate <- 2.683454548 # extinction rate
-  clade_carr_cap <- Inf # clade-level carrying capacity
-  imm_rate <- 0.00933207 # immigration rate
-  ana_rate <- 1.010073119 # anagenesis rate
-
-  expect_silent(
-    DAISIE_sim( 
-      time = island_age,
-      M = n_mainland_species, 
-      pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
-      replicates = n_replicates,
-      plot_sims = FALSE,
-      verbose = FALSE,
-      Apars = NULL
-    )
-  )
-})
