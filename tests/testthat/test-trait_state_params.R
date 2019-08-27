2390595658aaa06ffc5385536d7345f3eed54eec
xieshu95 <xieshu95@gmail.com>
2019-08-27 14:11:45 +0200

    create trait state parameters and test

---
+++ tests/testthat/test-trait_state_params.R
@@ -0,0 +1,13 @@
+context("create_trait_state_params")
+
+test_that("basic use", {
+  expect_silent(
+    create_trait_state_params(
+         trans_rate = 0.5,
+         immig_rate2 = 0.1,
+         ext_rate2 = 0.2,
+         ana_rate2 = 0.3,
+         clado_rate2 = 0.4,
+         trans_rate2 = 0.5,
+         M2 = 1000))
+})
