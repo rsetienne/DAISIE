7dc299c583e1f2f95f3031f9229afb6ea1ac8518
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-29 14:53:50 +0200

    Merge Josh's lintr work on Richel's branch, before moving it to develop

--- R/DAISIE_extract_stt_median.R
+++ R/DAISIE_extract_stt_median.R
@@ -13,16 +13,15 @@ DAISIE_extract_stt_median <- function(
   complete_arr <- array(dim = c(s_freq, 6, replicates))
   
   for (x in 1:replicates) {
-    sum_endemics <- island_replicates[[x]][[1]]$stt_all[, "nA"] + 
+    sum_endemics <- island_replicates[[x]][[1]]$stt_all[, "nA"] +
       island_replicates[[x]][[1]]$stt_all[, "nC"]
-    total <- island_replicates[[x]][[1]]$stt_all[, "nA"] + 
-             island_replicates[[x]][[1]]$stt_all[, "nC"] + 
+    total <- island_replicates[[x]][[1]]$stt_all[, "nA"] +
+             island_replicates[[x]][[1]]$stt_all[, "nC"] +
              island_replicates[[x]][[1]]$stt_all[, "nI"]
     complete_arr[, , x] <- cbind(
-      island_replicates[[x]][[1]]$stt_all[, c("Time", "nI", "nA", "nC")], 
-      sum_endemics, 
-      total
-    )
+      island_replicates[[x]][[1]]$stt_all[, c("Time", "nI", "nA", "nC")],
+      sum_endemics,
+      total)
   }
   stt_average_all <- apply(complete_arr, c(1, 2), stats::median)
   colnames(stt_average_all) <- c("Time", "nI", "nA", "nC", "Endemic", "Total")
