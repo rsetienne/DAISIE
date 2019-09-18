f1202897aa1a5f3dd53f3fe022b722943fc7b68e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-18 20:27:53 +0200

    Add first test for 'create_test_daisie_params' [skip ci]

--- tests/testthat/test-create_daisie_params.R
+++ tests/testthat/test-create_daisie_params.R
@@ -1,10 +1,17 @@
 test_that("use", {
-  expect_silent(
-    create_daisie_params(
-       time = 4,
-       M = 1,
-       pars = c(2.5, 2.6, Inf, 0.01, 1.0),
-       replicates = 1
-    )
+  time <- 3
+  M <- 1
+  pars <- c(2.5, 2.6, Inf, 0.01, 1.0)
+  replicates <- 1
+  daisie_params <- create_daisie_params(
+     time = time,
+     M = M,
+     pars = pars,
+     replicates = replicates
   )
+  expect_equal(daisie_params$time, time)
+  expect_equal(daisie_params$M, M)
+  expect_equal(daisie_params$pars, pars)
+  expect_equal(daisie_params$replicates, replicates)
+
 })
---
+++ tests/testthat/test-create_test_daisie_params.R
@@ -0,0 +1,12 @@
+test_that("use", {
+  daisie_params <- create_test_daisie_params(
+     time = time,
+     M = M,
+     pars = pars,
+     replicates = replicates
+  )
+  expect_equal(daisie_params$time, 3)
+  expect_equal(daisie_params$M, 1)
+  expect_equal(daisie_params$pars, c(2.5, 2.6, Inf, 0.01, 1.0))
+  expect_equal(daisie_params$replicates, 1)
+})
