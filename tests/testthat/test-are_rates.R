49468d7a0d5251fb0cb89c4c67e04bf4dc8e7a6a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-29 16:58:25 +0200

    Add 'are_rates'

--- tests/testthat/test-are_rates.R
+++ tests/testthat/test-are_rates.R
@@ -10,7 +10,28 @@ test_that("basic use", {
         clado_rate = 0.4,
         ext_rate_max = 0.5,
         immig_rate_max = 0.6,
-        clado_rate_max = 0.7
+        clado_rate_max = 0.7,
+        Tpars = list(trans_rate = 0.5, 
+                     immig_rate2 = 0.1, 
+                     ext_rate2 = 0.2, 
+                     ana_rate2 = 0.3, 
+                     clado_rate2 = 0.4, 
+                     trans_rate2 = 0.5, 
+                     M2 = 1000)
+      )
+    )
+  )
+  expect_true(
+    are_rates(
+      create_rates(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = 0.3,
+        clado_rate = 0.4,
+        ext_rate_max = 0.5,
+        immig_rate_max = 0.6,
+        clado_rate_max = 0.7,
+        Tpars = NULL
       )
     )
   )
