8c92bb543eb2affc35654e22e7b652f5f2a62ce0
xieshu95 <xieshu95@gmail.com>
2019-10-12 19:58:13 +0200

    test DAISEI vs TRASIE can create same stt_plot using same rates

---
+++ tests/testthat/test-DAISIE_vs_TRASIE.R
@@ -0,0 +1,73 @@
+context("DAISIE vs TRASEI")
+test_that("TRASIE can get the same result as DAISIE, for IW model", {
+
+  n_mainland_state1 = 1000
+  n_mainland_state2 = 1000
+  island_age <- 1
+  clado_rate <- 0.1 # cladogenesis rate
+  ext_rate <- 0.2 # extinction rate (not used)
+  island_wide_cap <- 100  # clade-level carrying capacity
+  imm_rate <- 0.01 # immigration rate
+  ana_rate <- 0.1 # anagenesis rate
+  trans_rate12 <- 0 # transition rate from state1 to state2
+  trans_rate21 <- 0 # transition rate from state2 to state1
+  pars = c(clado_rate, ext_rate, island_wide_cap, imm_rate, ana_rate)
+  pars_empty = c(0, 0, island_wide_cap, 0, 0)
+  Tpars_empty = create_trait_state_params(trans_rate = 0,
+                                    immig_rate2 = 0,
+                                    ext_rate2 = 0,
+                                    ana_rate2 = 0,
+                                    clado_rate2 = 0,
+                                    trans_rate2 = 0,
+                                    M2 = 0)
+  Tpars = create_trait_state_params(trans_rate = trans_rate12,
+                                          immig_rate2 = imm_rate,
+                                          ext_rate2 = ext_rate,
+                                          ana_rate2 = ana_rate,
+                                          clado_rate2 = clado_rate,
+                                          trans_rate2 = trans_rate21,
+                                          M2 = n_mainland_state2)
+  set.seed(1)
+  DAISIE_output <- DAISIE_sim(time = island_age,
+                              M = n_mainland_state1,
+                              pars = pars,
+                              Tpars = NULL,
+                              replicates=10,
+                              divdepmodel = "IW")
+  set.seed(1)
+  TRASIE_output1 <- DAISIE_sim(time = island_age,
+                               M = n_mainland_state1,
+                               pars = pars,
+                               Tpars = Tpars_empty,
+                               replicates=10,
+                               divdepmodel = "IW")
+  set.seed(1)
+  TRASIE_output2 <- DAISIE_sim(time = island_age,
+                               M = 0,
+                               pars = pars_empty,
+                               Tpars = Tpars,
+                               replicates=10,
+                               divdepmodel = "IW")
+  for(i in 1:length(DAISIE_output)){
+    for(j in 2:length(DAISIE_output[[i]])){
+      expect_identical(object = TRASIE_output1[[i]][[j]],expected = TRASIE_output2[[i]][[j]])
+    }
+    expect_identical(object = TRASIE_output1[[i]][[1]]$island_age,expected = TRASIE_output2[[i]][[1]]$island_age)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$not_present,expected = TRASIE_output2[[i]][[1]]$not_present)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$brts_table,expected = TRASIE_output2[[i]][[1]]$brts_table)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$stt_all[,1],expected = TRASIE_output2[[i]][[1]]$stt_all[,1])
+    # expect_identical(object = TRASIE_output1[[i]][[1]]$stt_all[1:26,2:4],
+    #                  expected = TRASIE_output2[[i]][[1]]$stt_all[1:26,5:7])
+  }
+  for(i in 1:length(DAISIE_output)){
+    for(j in 2:length(DAISIE_output[[i]])){
+      expect_identical(object = DAISIE_output[[i]][[j]],expected = TRASIE_output1[[i]][[j]])
+    }
+    expect_identical(object = DAISIE_output[[i]][[1]]$island_age,expected = TRASIE_output1[[i]][[1]]$island_age)
+    expect_identical(object = DAISIE_output[[i]][[1]]$not_present,expected = TRASIE_output1[[i]][[1]]$not_present)
+    expect_identical(object = DAISIE_output[[i]][[1]]$brts_table,expected = TRASIE_output1[[i]][[1]]$brts_table)
+    expect_identical(object = DAISIE_output[[i]][[1]]$stt_all[,1],expected = TRASIE_output1[[i]][[1]]$stt_all[,1])
+    expect_identical(object = DAISIE_output[[i]][[1]]$stt_all,
+                     expected = TRASIE_output1[[i]][[1]]$stt_all[,1:4])
+  }
+})
