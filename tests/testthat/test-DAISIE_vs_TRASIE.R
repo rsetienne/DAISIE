6fbce6c9e510e82bb4cb31c4dc73193726ca6f41
xieshu95 <xieshu95@gmail.com>
2019-10-15 01:03:00 +0200

    test DAISIE vs TRASIE and ML_IW

--- tests/testthat/test-DAISIE_vs_TRASIE.R
+++ tests/testthat/test-DAISIE_vs_TRASIE.R
@@ -6,7 +6,7 @@ test_that("TRASIE can get the same result as DAISIE, for IW model", {
   island_age <- 1
   clado_rate <- 0.1 # cladogenesis rate
   ext_rate <- 0.2 # extinction rate (not used)
-  island_wide_cap <- 100  # clade-level carrying capacity
+  island_wide_cap <- 100  # island_wide carrying capacity
   imm_rate <- 0.01 # immigration rate
   ana_rate <- 0.1 # anagenesis rate
   trans_rate12 <- 0 # transition rate from state1 to state2
@@ -69,5 +69,77 @@ test_that("TRASIE can get the same result as DAISIE, for IW model", {
     expect_identical(object = DAISIE_output[[i]][[1]]$stt_all[,1],expected = TRASIE_output1[[i]][[1]]$stt_all[,1])
     expect_identical(object = DAISIE_output[[i]][[1]]$stt_all,
                      expected = TRASIE_output1[[i]][[1]]$stt_all[,1:4])
+  }
+})
+test_that("TRASIE can get the same result as DAISIE, for CS model", {
+
+  n_mainland_state1 = 50
+  n_mainland_state2 = 50
+  island_age <- 1
+  clado_rate <- 0.1 # cladogenesis rate
+  ext_rate <- 0.2 # extinction rate (not used)
+  clade_spec_cap <- 10  # clade-level carrying capacity
+  imm_rate <- 1 # immigration rate
+  ana_rate <- 0.1 # anagenesis rate
+  trans_rate12 <- 0 # transition rate from state1 to state2
+  trans_rate21 <- 0 # transition rate from state2 to state1
+  pars = c(clado_rate, ext_rate, clade_spec_cap, imm_rate, ana_rate)
+  pars_empty = c(0, 0, clade_spec_cap, 0, 0)
+  Tpars_check = create_trait_state_params(trans_rate = trans_rate12,
+                                    immig_rate2 = imm_rate,
+                                    ext_rate2 = ext_rate,
+                                    ana_rate2 = ana_rate,
+                                    clado_rate2 = clado_rate,
+                                    trans_rate2 = trans_rate21,
+                                    M2 = n_mainland_state2)
+  Tpars_balance = create_trait_state_params(trans_rate = trans_rate12,
+                                            immig_rate2 = imm_rate,
+                                            ext_rate2 = ext_rate,
+                                            ana_rate2 = ana_rate,
+                                            clado_rate2 = clado_rate,
+                                            trans_rate2 = trans_rate21,
+                                            M2 = 25)
+  set.seed(1)
+  DAISIE_output <- DAISIE_sim(time = island_age,
+                              M = n_mainland_state1,
+                              pars = pars,
+                              Tpars = NULL,
+                              replicates=5,
+                              divdepmodel = "CS")
+  set.seed(1)
+  TRASIE_output1 <- DAISIE_sim(time = island_age,
+                               M = 0,
+                               pars = pars_empty,
+                               Tpars = Tpars_check,
+                               replicates=5,
+                               divdepmodel = "CS")
+  set.seed(1)
+  TRASIE_output2 <- DAISIE_sim(time = island_age,
+                               M = 25,
+                               pars = pars,
+                               Tpars = Tpars_balance,
+                               replicates=5,
+                               divdepmodel = "CS")
+  for(i in 1:length(DAISIE_output)){
+    for(j in 2:length(DAISIE_output[[i]])){
+      expect_identical(object = TRASIE_output1[[i]][[j]],expected = TRASIE_output2[[i]][[j]])
+    }
+    expect_identical(object = TRASIE_output1[[i]][[1]]$island_age,expected = TRASIE_output2[[i]][[1]]$island_age)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$not_present,expected = TRASIE_output2[[i]][[1]]$not_present)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$brts_table,expected = TRASIE_output2[[i]][[1]]$brts_table)
+    expect_identical(object = TRASIE_output1[[i]][[1]]$stt_all[,1],expected = TRASIE_output2[[i]][[1]]$stt_all[,1])
+    expect_identical(object = TRASIE_output1[[i]][[1]]$stt_all[,"present"],
+                     expected = TRASIE_output2[[i]][[1]]$stt_all[,"present"])
+  }
+  for(i in 1:length(DAISIE_output)){
+    for(j in 2:length(DAISIE_output[[i]])){
+      expect_identical(object = DAISIE_output[[i]][[j]],expected = TRASIE_output1[[i]][[j]])
+    }
+    expect_identical(object = DAISIE_output[[i]][[1]]$island_age,expected = TRASIE_output1[[i]][[1]]$island_age)
+    expect_identical(object = DAISIE_output[[i]][[1]]$not_present,expected = TRASIE_output1[[i]][[1]]$not_present)
+    expect_identical(object = DAISIE_output[[i]][[1]]$brts_table,expected = TRASIE_output1[[i]][[1]]$brts_table)
+    expect_identical(object = DAISIE_output[[i]][[1]]$stt_all[,1],expected = TRASIE_output1[[i]][[1]]$stt_all[,1])
+    expect_identical(object = DAISIE_output[[i]][[1]]$stt_all[,"present"],
+                     expected = TRASIE_output1[[i]][[1]]$stt_all[,"present"])
   }
 })
--- tests/testthat/test-IW_crash.R
+++ tests/testthat/test-IW_crash.R
@@ -1,6 +1,60 @@
+if (1 == 2) {
+  # Issue #81   estimate params using simulated data from DAISIE( Tpars = NULL), use ML_IW and ML_CS separately. Work!
+  test_that("general DAISIE_IW", {
+    pars1 <- c(0.5,0.2,20,0.002,0.1)
+    set.seed(100);islands_replicates_IW = DAISIE_sim(time=4,
+                                                     M=500,
+                                                     pars=pars1,
+                                                     Tpars = NULL,
+                                                     replicates=1,
+                                                     divdepmodel = "IW")
+    ###  estimate params using ML_CS
+    lambda_c <- factor()
+    mu <- factor()
+    K <- factor()
+    gamma <- factor()
+    lambda_a <- factor()
+    loglik <- factor()
+    df <- factor()
+    conv <- factor()
+    DAISIEML_CS_table <- data.frame(lambda_c,mu,K,gamma,lambda_a
+                                    ,loglik,df,conv)
+    for (i in 1:length(islands_replicates_IW)){
+      DAISIEML_CS <- DAISIE::DAISIE_ML_CS(datalist=islands_replicates_IW[[i]],
+                                          initparsopt = c(0.1,0.1,10,0.1,0.1),
+                                          ddmodel=0,
+                                          idparsopt = 1:5,
+                                          parsfix = NULL,
+                                          idparsfix = NULL)
+      DAISIEML_CS_table <- rbind(DAISIEML_CS_table, DAISIEML_CS)
+    }
+    # Calculating the likelihood for the initial parameters.
+    # The loglikelihood for the initial parameter values is -174.3363
+    # Optimizing the likelihood - this may take a while.
+    #
+    # Maximum likelihood parameter estimates: lambda_c: 0.190015, mu: 0.000000, K: 10.564105, gamma: 0.001000, lambda_a: 1650.628953
+    # Maximum loglikelihood: -18.476255
+
+    ###  estimate params using ML_IW
+    DAISIEML_IW_table <- data.frame(lambda_c,mu,K,gamma,lambda_a
+                                    ,loglik,df,conv)
+
+    for (i in 1:length(islands_replicates_IW)){
+      DAISIEML_IW <- DAISIE::DAISIE_ML_IW(datalist=islands_replicates_IW[[i]],
+                                          initparsopt = c(0.5,0.2,10,0.001,0.1),
+                                          ddmodel=0,
+                                          idparsopt = 1:5,
+                                          parsfix = NULL,
+                                          idparsfix = NULL)
+      DAISIEML_IW_table <- rbind(DAISIEML_IW_table, DAISIEML_IW)
+    }
+
+    # The loglikelihood for the initial parameter values is -19.5761 ,too lang time to get estimated values, so stop
+  })
+}
 ####  test when some of the values are zero    The crash probably caused by terrible initial value with 0s.
 if (1 == 2) {
-  # Issue #81  bot crash now, while cause numerical problem.
+  # Issue #81  not crash now, while cause numerical problem.
   test_that("TRASIE IW crash", {
     pars1 <- c(0,0,Inf,0.01,0)
     Tpars1 <- create_trait_state_params(trans_rate = 0,
@@ -36,6 +90,11 @@ if (1 == 2) {
                                           idparsfix = NULL)
       DAISIEML_IW_table <- rbind(DAISIEML_IW_table, DAISIEML_IW)
     }
+
+    # Calculating the likelihood for the initial parameters.
+    # The loglikelihood for the initial parameter values is -42.30258
+    # Optimizing the likelihood - this may take a while.
+    # There are parameter values used which cause numerical problems.
   })
 }
 
@@ -127,14 +186,21 @@ if (1 == 2) {
 }
 if (1 == 2) {
   # Issue #81
-  test_that("general DAISIE _IW", {
-    pars1 <- c(0.5,0.2,20,0.002,0.1)
-    set.seed(100);islands_replicates_IW = DAISIE_sim(time=4,
-                                                     M=500,
+  test_that("check CS", {
+    pars1 <- c(0.5,0.2,20,1,0.2)
+    Tpars1 <- create_trait_state_params(trans_rate = 0,
+                                        immig_rate2 = 1,
+                                        ext_rate2 = 0.2,
+                                        ana_rate2 = 0.2,
+                                        clado_rate2 = 0.5,
+                                        trans_rate2 = 0,
+                                        M2 = 10)
+    set.seed(100);islands_replicates_CS = DAISIE_sim(time=4,
+                                                     M=10,
                                                      pars=pars1,
-                                                     Tpars = NULL,
+                                                     Tpars = Tpars1,
                                                      replicates=1,
-                                                     divdepmodel = "IW")
+                                                     divdepmodel = "CS")
     lambda_c <- factor()
     mu <- factor()
     K <- factor()
@@ -145,6 +211,7 @@ if (1 == 2) {
     conv <- factor()
     DAISIEML_CS_table <- data.frame(lambda_c,mu,K,gamma,lambda_a
                                     ,loglik,df,conv)
+
     for (i in 1:length(islands_replicates_CS)){
       DAISIEML_CS <- DAISIE::DAISIE_ML_CS(datalist=islands_replicates_CS[[i]],
                                           initparsopt = c(0.1,0.1,10,0.1,0.1),
@@ -154,53 +221,5 @@ if (1 == 2) {
                                           idparsfix = NULL)
       DAISIEML_CS_table <- rbind(DAISIEML_CS_table, DAISIEML_CS)
     }
-    DAISIEML_IW_table <- data.frame(lambda_c,mu,K,gamma,lambda_a
-                                    ,loglik,df,conv)
-
-    for (i in 1:length(islands_replicates_IW)){
-      DAISIEML_IW <- DAISIE::DAISIE_ML_IW(datalist=islands_replicates_IW[[i]],
-                                          initparsopt = c(0.5,0.2,10,0.001,0.1),
-                                          ddmodel=0,
-                                          idparsopt = 1:5,
-                                          parsfix = NULL,
-                                          idparsfix = NULL)
-      DAISIEML_IW_table <- rbind(DAISIEML_IW_table, DAISIEML_IW)
-    }
   })
 }
-test_that("check CS", {
-  pars1 <- c(0.5,0.2,20,1,0.2)
-  Tpars1 <- create_trait_state_params(trans_rate = 0,
-                                      immig_rate2 = 1,
-                                      ext_rate2 = 0.2,
-                                      ana_rate2 = 0.2,
-                                      clado_rate2 = 0.5,
-                                      trans_rate2 = 0,
-                                      M2 = 10)
-  set.seed(100);islands_replicates_CS = DAISIE_sim(time=4,
-                                                   M=10,
-                                                   pars=pars1,
-                                                   Tpars = Tpars1,
-                                                   replicates=1,
-                                                   divdepmodel = "CS")
-  lambda_c <- factor()
-  mu <- factor()
-  K <- factor()
-  gamma <- factor()
-  lambda_a <- factor()
-  loglik <- factor()
-  df <- factor()
-  conv <- factor()
-  DAISIEML_CS_table <- data.frame(lambda_c,mu,K,gamma,lambda_a
-                               ,loglik,df,conv)
-
-  for (i in 1:length(islands_replicates_CS)){
-    DAISIEML_CS <- DAISIE::DAISIE_ML_CS(datalist=islands_replicates_CS[[i]],
-                                        initparsopt = c(0.1,0.1,10,0.1,0.1),
-                                        ddmodel=0,
-                                        idparsopt = 1:5,
-                                        parsfix = NULL,
-                                        idparsfix = NULL)
-    DAISIEML_CS_table <- rbind(DAISIEML_CS_table, DAISIEML_CS)
-  }
-})
