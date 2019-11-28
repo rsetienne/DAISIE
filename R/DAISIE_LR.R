d37e01145cdc429f990440d190969d4ea8cbf63f
joshwlambert <j.w.l.lambert@rug.nl>
2019-11-28 09:27:36 +0100

    changes to DAISIE_LR and test-DAISIE_LR

--- R/DAISIE_LR.R
+++ R/DAISIE_LR.R
@@ -1,3 +1,33 @@
+#' Bootstrap likelihood ratio test
+#'
+#' @param datalist stub
+#' @param datatype stub
+#' @param initparsopt_dd stub
+#' @param initparsopt_di stub
+#' @param idparsnoshift stub
+#' @param idparsmat stub
+#' @param endmc stub
+#' @param seed stub
+#' @param alpha stub
+#' @param res stub
+#' @param ddmodel stub
+#' @param cond stub
+#' @param island_ontogeny stub
+#' @param eqmodel stub
+#' @param x_E stub
+#' @param x_I stub
+#' @param tol stub
+#' @param maxiter stub
+#' @param methode stub
+#' @param optimmethod stub
+#' @param CS_version stub
+#' @param verbose stub
+#' @param tolint stub
+#'
+#' @return stub
+#' @export
+#'
+#' @examples stub
 DAISIE_LR <- function(
   datalist,
   datatype = "single",
@@ -6,6 +36,8 @@ DAISIE_LR <- function(
   idparsnoshift = 6:10,
   idparsmat = NULL,
   endmc = 1000,
+  seed = 17,
+  alpha = 0.05,
   res = 100,
   ddmodel = 0,
   cond = 0,
@@ -20,53 +52,73 @@ DAISIE_LR <- function(
   CS_version = 1,
   verbose = TRUE,
   tolint = c(1E-16, 1E-10)) {
-    if (verbose == TRUE) {
-      cat("Estimating parameters under the diversity-independent model \n")
-    }
-    init_di_ml <- DAISIE_ML_CS(datalist = datalist,
-                               datatype = datatype,
-                               initparsopt = initparsopt_di,
-                               idparsopt = c(1, 2, 4, 5),
-                               parsfix = Inf,
-                               idparsfix = 3,
-                               idparsnoshift = idparsnoshift,
-                               idparsmat = idparsmat,
-                               res = res,
-                               ddmodel = ddmodel, #should this be set to zero or does it not matter when K is set to inf?
-                               cond = cond,
-                               island_ontogeny = island_ontogeny,
-                               eqmodel = eqmodel,
-                               x_E = x_E,
-                               x_I = x_I,
-                               tol = tol,
-                               maxiter = maxiter,
-                               methode = methode,
-                               optimmethod = optimmethod,
-                               CS_version = CS_version,
-                               verbose = verbose,
-                               tolint = tolint)
-    if (verbose == TRUE) {
-      cat("Estimating parameters under the diversity-dependent model \n")
-    }
-    init_dd_ml <- DAISIE_ML_CS(datalist = datalist,
-                               datatype = datatype,
-                               initparsopt = initparsopt_dd,
-                               idparsopt = 1:5,
-                               parsfix = NULL,
-                               idparsfix = NULL,
-                               idparsnoshift = idparsnoshift,
-                               idparsmat = idparsmat,
-                               res = res,
-                               ddmodel = ddmodel,
-                               cond = cond,
-                               island_ontogeny = island_ontogeny,
-                               eqmodel = eqmodel,
-                               x_E = x_E,
-                               x_I = x_I,tol = tol,
-                               maxiter = maxiter,
-                               methode = methode,
-                               optimmethod = optimmethod,
-                               CS_version = CS_version,
-                               verbose = verbose,
-                               tolint = tolint)
-    }
+  if (verbose == TRUE) {
+    cat("Estimating parameters under the diversity-independent model \n")
+  }
+  init_di_ml <- DAISIE_ML_CS(datalist = datalist,
+                             datatype = datatype,
+                             initparsopt = initparsopt_di,
+                             idparsopt = c(1, 2, 4, 5),
+                             parsfix = Inf,
+                             idparsfix = 3,
+                             idparsnoshift = idparsnoshift,
+                             idparsmat = idparsmat,
+                             res = res,
+                             ddmodel = ddmodel, #should this be set to zero or does it not matter when K is set to inf?
+                             cond = cond,
+                             island_ontogeny = island_ontogeny,
+                             eqmodel = eqmodel,
+                             x_E = x_E,
+                             x_I = x_I,
+                             tol = tol,
+                             maxiter = maxiter,
+                             methode = methode,
+                             optimmethod = optimmethod,
+                             CS_version = CS_version,
+                             verbose = verbose,
+                             tolint = tolint)
+  if (verbose == TRUE) {
+    cat("Estimating parameters under the diversity-dependent model \n")
+  }
+  init_dd_ml <- DAISIE_ML_CS(datalist = datalist,
+                             datatype = datatype,
+                             initparsopt = initparsopt_dd,
+                             idparsopt = 1:5,
+                             parsfix = NULL,
+                             idparsfix = NULL,
+                             idparsnoshift = idparsnoshift,
+                             idparsmat = idparsmat,
+                             res = res,
+                             ddmodel = ddmodel,
+                             cond = cond,
+                             island_ontogeny = island_ontogeny,
+                             eqmodel = eqmodel,
+                             x_E = x_E,
+                             x_I = x_I,tol = tol,
+                             maxiter = maxiter,
+                             methode = methode,
+                             optimmethod = optimmethod,
+                             CS_version = CS_version,
+                             verbose = verbose,
+                             tolint = tolint)
+  likelihood_ratio_zero = init_dd_ml$loglik - init_di_ml$loglik
+
+  di_pars <- as.numeric(outCRO[1:2])
+  dd_pars <- as.numeric(outDDO[1:3])
+  di_tree <- list()
+  dd_tree <- list()
+
+  cat('\nSimulating trees under CR and DD models ...\n')
+  for(mc in 1:endmc) {
+    di_tree[[mc]] = DAISIE_sim(time = ,
+                               M = ,
+                               pars = di_pars,
+                               replicates = )
+
+    dd_tree[[mc]] = DAISIE_sim(time = ,
+                               M = ,
+                               pars = di_pars,
+                               replicates = )
+
+  }
+}
--- tests/testthat/test-DAISIE_LR.R
+++ tests/testthat/test-DAISIE_LR.R
@@ -1,7 +1,12 @@
 test_that("bootstrapping is silent", {
-  expect_silent()
+  skip("WIP")
+  utils::data(Galapagos_datalist)
+  expect_silent(DAISIE_LR(datalist = Galapagos_datalist,
+                          initparsopt_dd = c(2.5,2.7,20,0.009,1.01),
+                          initparsopt_di = c(2.5,2.7,0.009,1.01)))
 })
 
 test_that("calculates maximum likelihood for data for each model", {
+  skip("WIP")
   expect_true()
 })
