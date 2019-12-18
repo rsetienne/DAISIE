1d3c4d8554e9d11005ec2961396fd42e6af019f8
Neves-P <p.m.santos.neves@rug.nl>
2019-12-18 10:57:21 +0100

    Test rate and input checking functions

--- tests/testthat/test-are_rates.R
+++ tests/testthat/test-are_rates.R
@@ -35,4 +35,106 @@ test_that("check returns FALSE when wrong", {
       )
     )
   )
+  expect_false(
+    are_rates(
+      list(
+        clado_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = 0.3
+      )
+    )
+  )
+
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = 0.3,
+        clado_rate = "nonsense"
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        clado_rate = 0.1,
+        immig_rate = 0.2,
+        ana_rate = 0.3
+      )
+    )
+  )
+
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = "nonsense",
+        ana_rate = 0.3,
+        clado_rate = 0.4
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        clado_rate = 0.3
+      )
+    )
+  )
+
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = "nonsense",
+        clado_rate = 0.4
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = -1,
+        clado_rate = 0.4
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = -1,
+        ext_rate = 0.2,
+        ana_rate = 0.2,
+        clado_rate = 0.4
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = -1,
+        ana_rate = 0.3,
+        clado_rate = 0.4
+      )
+    )
+  )
+  expect_false(
+    are_rates(
+      list(
+        immig_rate = 0.1,
+        ext_rate = 0.2,
+        ana_rate = 0.5,
+        clado_rate = -1
+      )
+    )
+  )
 })
+
+
