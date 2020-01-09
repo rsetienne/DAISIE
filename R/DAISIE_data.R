197fcc032a052c76ef817347836e77e5746ce7e5
rsetienne <rampaletienne@gmail.com>
2020-01-09 23:01:47 +0100

    Simulations with the MW model.

--- R/DAISIE_data.R
+++ R/DAISIE_data.R
@@ -9,7 +9,7 @@
 #' colonists. This list can be generated using the DAISIE_dataprep function,
 #' which converts a user-specified data table into a data object, but the
 #' object can of course also be entered directly. It is an R list object with
-#' the following elements.\cr \cr 
+#' the following elements.\cr \cr
 #' The first element of the list has two
 #' components: \cr \cr
 #' \code{$island_age} - the island age \cr
@@ -122,7 +122,7 @@ NULL
 
 #' @name islands_10reps_RAW
 #' @title 1000 islands in RAW format simulated with the ML parameters of the CR model
-#' for the Galapagos data. 
+#' for the Galapagos data.
 #' @format A list with 10 items.
 #' @description Each simulated dataset is an element of the list, which can be called using
 #' e.g. islands_10reps_RAW[[1]] Each of the island replicates is a list in
@@ -162,7 +162,7 @@ NULL
 #' e.g. islands_1type_1000reps[[1]] Each of the island replicates is a list in
 #' itself. The first (e.g. islands_1type_1000reps[[x]][[1]]) element of that
 #' list has the following components: \cr
-#' \code{$island_age} - the island age\cr 
+#' \code{$island_age} - the island age\cr
 #' \code{$not_present} - the number of mainland lineages that are not
 #' present on the island \cr
 #' \code{$stt_all} - STT table for all species on the
@@ -287,12 +287,12 @@ NULL
 #' @docType data
 #' @format A list with 69 elements the first of which contains 2 elements and
 #' the following 68 containing 5 components.
-#' #' @description A list containing the colonization and branching times of the 
+#' #' @description A list containing the colonization and branching times of the
 #' Lake Biwa (Japan) fishes where no distinction is made between types of
 #' colonists. This list can be generated using the DAISIE_dataprep function,
 #' which converts a user-specified data table into a data object, but the
 #' object can of course also be entered directly. It is an R list object with
-#' the following elements.\cr \cr 
+#' the following elements.\cr \cr
 #' The first element of the list has two
 #' components: \cr \cr
 #' \code{$island_age} - the island age \cr
@@ -319,8 +319,17 @@ NULL
 #' \code{$type1or2} - whether the colonist belongs to type 1 or
 #' type 2. In this dataset all are equal to 1. \cr
 #' @seealso \code{\link{DAISIE_dataprep}}, \code{\link{DAISIE_ML}}, \code{\link{DAISIE_SR_ML}}
-#' @source Hauffe, T., D. Delicado, R.S. Etienne and L. Valente (submitted). 
+#' @source Hauffe, T., D. Delicado, R.S. Etienne and L. Valente (submitted).
 #' Lake expansion increases equilibrium diversity via the target effect of
 #' island biogeography
+#' @keywords datasets
+NULL
+
+#' @name archipelago_data
+#' @title data on 41 archipelagos
+#' @docType data
+#' @format A dataframe containing information on archipelago name, area, age and distance from the mainland
+#' @description A dataframe with in subsequent columns the name of the archipelago (Archipelago)
+#' the area of the archipelago (Area), the age (Age) and the distance from the mainland (Distance)
 #' @keywords datasets
 NULL
