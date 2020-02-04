5a184ddd72be7be6f361747de935a10d6e719f39
Neves-P <p.m.santos.neves@rug.nl>
2020-02-04 19:17:07 +0100

    Vignettes build

--- vignettes/demo_sim.R
+++ vignettes/demo_sim.R
@@ -24,14 +24,30 @@ clade_carr_cap <- Inf # clade-level carrying capacity
 imm_rate <- 0.00933207 # immigration rate
 ana_rate <- 1.010073119 # anagenesis rate
 
-island_replicates <- DAISIE_sim( 
+island_replicates <- DAISIE_sim_constant_rate( 
   time = island_age,
   M = n_mainland_species, 
   pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
   replicates = n_replicates,
   plot_sims = FALSE,
-  verbose = FALSE,
-  area_pars = NULL
+  verbose = FALSE
+)
+DAISIE_plot_sims(island_replicates = island_replicates)
+
+## ----fig.width=7, fig.height=7------------------------------------------------
+clado_rate <- 2.550687345 # cladogenesis rate
+ext_rate <- 2.683454548 # extinction rate
+clade_carr_cap <- Inf # clade-level carrying capacity
+imm_rate <- 0.00933207 # immigration rate
+ana_rate <- 1.010073119 # anagenesis rate
+
+island_replicates <- DAISIE_sim_constant_rate( 
+  time = island_age,
+  M = n_mainland_species, 
+  pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
+  replicates = n_replicates,
+  plot_sims = FALSE,
+  verbose = FALSE
 )
 DAISIE_plot_sims(island_replicates = island_replicates)
 
@@ -42,7 +58,7 @@ clade_carr_cap <- 10.0  # clade-level carrying capacity
 imm_rate <- 0.00933207 # immigration rate
 ana_rate <- 1.010073119 # anagenesis rate
 
-island_replicates_K <- DAISIE_sim( 
+island_replicates_K <- DAISIE_sim_constant_rate( 
   time = island_age, 
   M = n_mainland_species, 
   pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
@@ -65,7 +81,7 @@ clade_carr_cap_2 <- clade_carr_cap_1  # clade-level carrying capacity
 imm_rate_2 <- imm_rate_1 # immigration rate
 ana_rate_2 <- ana_rate_1 # anagenesis rate
 
-island_replicates_2types <- DAISIE_sim( 
+island_replicates_2types <- DAISIE_sim_constant_rate( 
   time = island_age,
   M = n_mainland_species, 
   pars = c(
