78118292f531f068fe8936537aacee71fe4025c8
Neves-P <p.m.santos.neves@rug.nl>
2020-02-09 23:04:19 +0100

    Expand tests core_time_dependent and create_area_params

--- tests/testthat/test-create_area_params.R
+++ tests/testthat/test-create_area_params.R
@@ -12,5 +12,37 @@ test_that("minimal use", {
       island_gradient_angle = 45
     )
   )
+  out <- create_area_pars(
+    max_area = 10,
+    proportional_peak_t = 0.5,
+    peak_sharpness = 1,
+    total_island_age = 5,
+    sea_level_amplitude = 5,
+    sea_level_frequency = 10,
+    island_gradient_angle = 45
+  )
+  reference <- list(
+    max_area = 10,
+    proportional_peak_t = 0.5,
+    peak_sharpness = 1,
+    total_island_age = 5,
+    sea_level_amplitude = 5,
+    sea_level_frequency = 10,
+    island_gradient_angle = 45
+  )
+  expect_equal(out, reference)
+})
 
+test_that("abuse", {
+  expect_error(
+    create_area_pars(
+      max_area = 0,
+      proportional_peak_t = 0.5,
+      peak_sharpness = 1,
+      total_island_age = 5,
+      sea_level_amplitude = 5,
+      sea_level_frequency = 10,
+      island_gradient_angle = 45
+    ), regexp = "max_area > 0 is not TRUE"
+  )
 })
