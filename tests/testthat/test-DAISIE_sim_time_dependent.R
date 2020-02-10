e7d6c9a7e40dca87cc2e6b6d54dcf4e740082259
joshwlambert <j.w.l.lambert@rug.nl>
2020-02-10 18:03:46 +0100

    Added time-dependent tests for IW and GW

--- tests/testthat/test-DAISIE_sim_time_dependent.R
+++ tests/testthat/test-DAISIE_sim_time_dependent.R
@@ -1,6 +1,6 @@
 context("DAISIE_sim_time_dependent")
 
-test_that("A clean ontogeny run should produce no output", {
+test_that("A clean CS ontogeny run should produce no output", {
   n_mainland_species <- 1000
   island_age <- 0.4
   clado_rate <- 0.0001 # cladogenesis rate
@@ -42,6 +42,99 @@ test_that("A clean ontogeny run should produce no output", {
     )
   )
 })
+
+test_that("A clean IW ontogeny run should produce no output", {
+  n_mainland_species <- 1000
+  island_age <- 0.4
+  clado_rate <- 0.0001 # cladogenesis rate
+  ext_rate <- 2.683454548 # extinction rate (not used)
+  clade_carr_cap <- 0.05  # clade-level carrying capacity
+  imm_rate <- 0.001 # immigration rate
+  ana_rate <- 0.1 # anagenesis rate
+  divdepmodel <- "IW"
+  max_area <- 1000
+  peak_time <- 0.1
+  sharpness <- 1
+  total_island_age <- 10
+  sea_level_amplitude <- 0
+  sea_level_frequency <- 0
+  island_gradient_angle <- 0
+  mu_min <- 0.5
+  mu_max <- 100
+  island_ontogeny <- "beta"
+  sea_level <- "const"
+  extcutoff <- 1000
+  expect_silent(
+    DAISIE_sim_time_dependent(
+      time = island_age,
+      M = n_mainland_species,
+      pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
+      replicates = 1,
+      divdepmodel = divdepmodel,
+      island_ontogeny = island_ontogeny,
+      sea_level = sea_level,
+      area_pars = create_area_pars(max_area,
+                                   peak_time,
+                                   sharpness,
+                                   total_island_age,
+                                   sea_level_amplitude,
+                                   sea_level_frequency,
+                                   island_gradient_angle),
+      ext_pars = c(mu_min, mu_max),
+      extcutoff = extcutoff,
+      plot_sims = FALSE,
+      verbose = FALSE
+    )
+  )
+})
+
+test_that("A clean GW ontogeny run should produce no output", {
+  n_mainland_species <- 1000
+  island_age <- 0.4
+  clado_rate <- 0.0001 # cladogenesis rate
+  ext_rate <- 2.683454548 # extinction rate (not used)
+  clade_carr_cap <- 0.05  # clade-level carrying capacity
+  imm_rate <- 0.001 # immigration rate
+  ana_rate <- 0.1 # anagenesis rate
+  divdepmodel <- "GW"
+  num_guilds <- 10
+  max_area <- 1000
+  peak_time <- 0.1
+  sharpness <- 1
+  total_island_age <- 10
+  sea_level_amplitude <- 0
+  sea_level_frequency <- 0
+  island_gradient_angle <- 0
+  mu_min <- 0.5
+  mu_max <- 100
+  island_ontogeny <- "beta"
+  sea_level <- "const"
+  extcutoff <- 1000
+  expect_silent(
+    DAISIE_sim_time_dependent(
+      time = island_age,
+      M = n_mainland_species,
+      pars = c(clado_rate, ext_rate, clade_carr_cap, imm_rate, ana_rate),
+      replicates = 1,
+      divdepmodel = divdepmodel,
+      num_guilds = num_guilds,
+      island_ontogeny = island_ontogeny,
+      sea_level = sea_level,
+      area_pars = create_area_pars(max_area,
+                                   peak_time,
+                                   sharpness,
+                                   total_island_age,
+                                   sea_level_amplitude,
+                                   sea_level_frequency,
+                                   island_gradient_angle),
+      ext_pars = c(mu_min, mu_max),
+      extcutoff = extcutoff,
+      plot_sims = FALSE,
+      verbose = FALSE
+    )
+  )
+})
+
 
 test_that("A clean sea_level run should produce no output", {
   n_mainland_species <- 1000
