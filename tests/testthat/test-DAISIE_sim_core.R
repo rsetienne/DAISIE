c7bae6c78412f83e1d1dc9dd33162e18e49788a2
rsetienne <rampaletienne@gmail.com>
2020-03-06 12:54:32 +0100

    Adding testthat::

--- tests/testthat/test-DAISIE_sim_core.R
+++ tests/testthat/test-DAISIE_sim_core.R
@@ -1,42 +1,42 @@
 context("DAISIE_sim_core")
 
 test_that("new and v1.4 should give same results", {
-  
+
   sim_time <- 10
-  n_mainland_species <- 1 
+  n_mainland_species <- 1
   clado_rate <- 1.0
   ext_rate <- 0.5
-  carr_cap <- 10 
+  carr_cap <- 10
   imm_rate <- 1.0
   ana_rate <- 1.0
   pars <- c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate)
   rng_seed <- 42
   set.seed(rng_seed)
   new <- DAISIE:::DAISIE_sim_core(
-    time = sim_time, 
-    mainland_n = n_mainland_species, 
+    time = sim_time,
+    mainland_n = n_mainland_species,
     pars = pars
   )
   set.seed(rng_seed)
   old <- DAISIE:::DAISIE_sim_core_1_4(
-    time = sim_time, 
-    mainland_n = n_mainland_species, 
+    time = sim_time,
+    mainland_n = n_mainland_species,
     pars = pars
   )
-  
-  expect_true(all(names(new) == names(old)))
-  # stt_table has different content
-  expect_true(nrow(new$stt_table) == nrow(old$stt_table))
-  # different branching times
-  expect_equal(length(new$branching_times), length(old$branching_times))
-  expect_true(new$stac == old$stac)
-  expect_true(new$missing_species == old$missing_species)
-  expect_true(length(new$other_clades_same_ancestor) == length(old$other_clades_same_ancestor))
-  expect_true(new$other_clades_same_ancestor[[1]]$species_type == old$other_clades_same_ancestor[[1]]$species_type)
 
-  expect_true(all(new$stt_table == old$stt_table))
-  expect_true(all(new$branching_times == old$branching_times))
-  expect_true(new$other_clades_same_ancestor[[1]]$brts_miss == old$other_clades_same_ancestor[[1]]$brts_miss)
+  testthat::expect_true(all(names(new) == names(old)))
+  # stt_table has different content
+  testthat::expect_true(nrow(new$stt_table) == nrow(old$stt_table))
+  # different branching times
+  testthat::expect_equal(length(new$branching_times), length(old$branching_times))
+  testthat::expect_true(new$stac == old$stac)
+  testthat::expect_true(new$missing_species == old$missing_species)
+  testthat::expect_true(length(new$other_clades_same_ancestor) == length(old$other_clades_same_ancestor))
+  testthat::expect_true(new$other_clades_same_ancestor[[1]]$species_type == old$other_clades_same_ancestor[[1]]$species_type)
+
+  testthat::expect_true(all(new$stt_table == old$stt_table))
+  testthat::expect_true(all(new$branching_times == old$branching_times))
+  testthat::expect_true(new$other_clades_same_ancestor[[1]]$brts_miss == old$other_clades_same_ancestor[[1]]$brts_miss)
 })
 
 test_that("Clean run should be silent", {
@@ -44,13 +44,13 @@ test_that("Clean run should be silent", {
   set.seed(42)
   n_mainland_species <- 1
   sim_time <- 10
-  clado_rate <- 1.0 
+  clado_rate <- 1.0
   ext_rate <- 0.1
   carr_cap <- 4
   imm_rate <- 1.0
   ana_rate <- 1.0
-  
-  expect_silent(
+
+  testthat::expect_silent(
     DAISIE:::DAISIE_sim_core(
       time = sim_time,
       mainland_n = n_mainland_species,
@@ -76,7 +76,7 @@ test_that("Pedro's should run silent", {
     Epars = c(1, 100),
     island_ontogeny = "beta"
   )
-  expect_silent(
+  testthat::expect_silent(
     DAISIE:::DAISIE_sim_core(
       time = 10,
       mainland_n = 1,
@@ -103,8 +103,8 @@ test_that("all species extinct if island dead", {
                     island_ontogeny = "beta"
   )
   last_entry <- ontogeny_sim$stt_table[nrow(ontogeny_sim$stt_table),]
-  expect_true(last_entry[1] == 0)
-  expect_true(last_entry[2] == 0)
-  expect_true(last_entry[3] == 0)
-  expect_true(last_entry[4] == 0)
+  testthat::expect_true(last_entry[1] == 0)
+  testthat::expect_true(last_entry[2] == 0)
+  testthat::expect_true(last_entry[3] == 0)
+  testthat::expect_true(last_entry[4] == 0)
 })
