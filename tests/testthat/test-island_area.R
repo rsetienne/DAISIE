abbb94be6b0b060e922ed98d743568cc40a4feeb
Neves-P <p.m.santos.neves@rug.nl>
2020-04-20 15:17:39 +0200

    Fix test-island_area

--- tests/testthat/test-island_area.R
+++ tests/testthat/test-island_area.R
@@ -4,8 +4,8 @@ test_that("is valid island area with constant area", {
   area <- island_area(timeval = 2,
                       area_pars <- create_area_pars(
                         max_area = 1,
+                        current_area = 1,
                         proportional_peak_t = 0,
-                        peak_sharpness = 0,
                         total_island_age = 5,
                         sea_level_amplitude = 0,
                         sea_level_frequency = 0,
@@ -17,15 +17,20 @@ test_that("is valid island area with constant area", {
 })
 
 test_that("is valid island area with ontogeny", {
+  area_pars <- create_area_pars(
+    max_area = 10,
+    current_area = 5,
+    proportional_peak_t = 0.5,
+    total_island_age = 5,
+    sea_level_amplitude = 0,
+    sea_level_frequency = 0,
+    island_gradient_angle = 0
+  )
+  peak <- DAISIE:::calc_peak(totaltime = 4, area_pars = area_pars)
+
   area <- island_area(timeval = 2,
-                      area_pars = create_area_pars(
-                        max_area = 10,
-                        proportional_peak_t = 0.5,
-                        peak_sharpness = 1,
-                        total_island_age = 5,
-                        sea_level_amplitude = 0,
-                        sea_level_frequency = 0,
-                        island_gradient_angle = 0),
+                      area_pars = area_pars,
+                      peak = peak,
                       island_ontogeny = translate_island_ontogeny("beta"),
                       sea_level = translate_sea_level("const")
   )
@@ -35,13 +40,13 @@ test_that("is valid island area with ontogeny", {
 test_that("is valid island area with sea level", {
   area <- island_area(timeval = 2,
                       area_pars = create_area_pars(
-                        max_area = 10,
+                        max_area = 1000,
+                        current_area = 1,
                         proportional_peak_t = 0,
-                        peak_sharpness = 0,
-                        total_island_age = 5,
-                        sea_level_amplitude = 2,
-                        sea_level_frequency = 10,
-                        island_gradient_angle = 45),
+                        total_island_age = 10,
+                        sea_level_amplitude = 60,
+                        sea_level_frequency = 3,
+                        island_gradient_angle = 85),
                       island_ontogeny = translate_island_ontogeny("const"),
                       sea_level = translate_sea_level("sine")
   )
@@ -49,15 +54,19 @@ test_that("is valid island area with sea level", {
 })
 
 test_that("is valid island area with ontogeny and sea level", {
+  area_pars <- create_area_pars(
+    max_area = 10,
+    current_area = 2,
+    proportional_peak_t = 0.5,
+    total_island_age = 5,
+    sea_level_amplitude = 2,
+    sea_level_frequency = 10,
+    island_gradient_angle = 85
+  )
+  peak <- DAISIE:::calc_peak(totaltime = 4, area_pars = area_pars)
   area <- island_area(timeval = 2,
-                      area_pars = create_area_pars(
-                        max_area = 10,
-                        proportional_peak_t = 0.5,
-                        peak_sharpness = 1,
-                        total_island_age = 5,
-                        sea_level_amplitude = 2,
-                        sea_level_frequency = 10,
-                        island_gradient_angle = 45),
+                      area_pars = area_pars,
+                      peak = peak,
                       island_ontogeny = translate_island_ontogeny("beta"),
                       sea_level = translate_sea_level("sine")
   )
@@ -68,8 +77,8 @@ test_that("abuse island area with constant area", {
   expect_warning(island_area(timeval = 2,
                              area_pars = create_area_pars(
                                max_area = 10,
+                               current_area = 1,
                                proportional_peak_t = 0.5,
-                               peak_sharpness = 1,
                                total_island_age = 5,
                                sea_level_amplitude = 5,
                                sea_level_frequency = 10,
