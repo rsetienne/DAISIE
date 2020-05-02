a638040ab57c49e78e894c7f319ad21a6082d1fa
joshwlambert <j.w.l.lambert@rug.nl>
2020-05-02 14:57:57 +0100

    updated and vectorised DAISIE_loglik_integrate

--- R/DAISIE_loglik_integrate.R
+++ R/DAISIE_loglik_integrate.R
@@ -25,33 +25,38 @@ DAISIE_loglik_integrate <- function(
   # For most purposes values of -4.01 (lognormal on K, with sd of 0.1) or +4.2 (exponential) or +4.3
   # (gamma with shape 2) are sufficient.
   if(sign(CS_version) == -1) {
-    rho <- function(DAISIE_par, DAISIE_par_dist_pars) {
-      return(dlnorm(x = DAISIE_par,
-                    meanlog = log(DAISIE_par_dist_pars[1]) - (DAISIE_par_dist_pars[2]^2)/2,
-                    sdlog = sqrt(log(1 + DAISIE_par_dist_pars[2] / DAISIE_par_dist_pars[1]^2))))
+    rho <- function(DAISIE_par, DAISIE_dist_pars) {
+      return(stats::dlnorm(x = DAISIE_par,
+                           meanlog = log(DAISIE_dist_pars[1]) - (DAISIE_dist_pars[2]^2)/2,
+                           sdlog = sqrt(log(1 + DAISIE_dist_pars[2] / DAISIE_dist_pars[1]^2))))
     }
   } else {
-    rho <- function(DAISIE_par, DAISIE_par_dist_pars) {
-      return(dgamma(x = DAISIE_par,
-                    shape = DAISIE_par_dist_pars[2]^2,
-                    scale = DAISIE_par_dist_pars[1]/DAISIE_par_dist_pars[2]^2))
+    rho <- function(DAISIE_par, DAISIE_dist_pars) {
+      return(stats::dgamma(x = DAISIE_par,
+                           shape = DAISIE_dist_pars[2]^2,
+                           scale = DAISIE_dist_pars[1]/DAISIE_dist_pars[2]^2))
     }
-    if (CS_version == 2) {
-      CS_version <- 2.3
-    }
-    # if CS_version = x.2, then exponential distribution
-    # if CS_version = x.3, then gamma distribution with shape 2
   }
 
   k_or_sigma <- 10 * (abs(CS_version) - floor(abs(CS_version)))
   pick <- floor(abs(CS_version)) - 1
   mean_par <- pars1[pick]
-  pars1new <- pars1
 
-  DAISIE_loglik_integrand <- function(DAISIE_par) {
-    pars1new[pick] <- DAISIE_par
+  DAISIE_loglik_integrand <- function(DAISIE_par,
+                                      pars1,
+                                      pars2,
+                                      brts,
+                                      stac,
+                                      missnumspec,
+                                      methode,
+                                      abstolint,
+                                      reltolint,
+                                      verbose,
+                                      mean_par,
+                                      k_or_sigma) {
+    pars1[pick] <- DAISIE_par
     loglik_DAISIE_par <- exp(DAISIE_loglik(
-      pars1 = pars1new,
+      pars1 = pars1,
       pars2 = pars2,
       brts = brts,
       stac = stac,
@@ -63,14 +68,27 @@ DAISIE_loglik_integrate <- function(
       ) *
       rho(
         DAISIE_par = DAISIE_par,
-        DAISIE_par_dist_pars = c(mean_par,k_or_sigma)
+        DAISIE_dist_pars = c(mean_par,k_or_sigma)
       )
     return(loglik_DAISIE_par)
   }
 
-  integrated_loglik <- log(integrate(f = DAISIE_loglik_integrand,
-                                     lower = 0,
-                                     upper = Inf
-  ))
+  integrated_loglik <- log(stats::integrate(
+    f = Vectorize(DAISIE_loglik_integrand,
+                  vectorize.args = "DAISIE_par"),
+    lower = 0,
+    upper = Inf,
+    pars1 = pars1,
+    pars2 = pars2,
+    brts = brts,
+    stac = stac,
+    missnumspec = missnumspec,
+    methode = methode,
+    abstolint = abstolint,
+    reltolint = reltolint,
+    verbose = verbose,
+    mean_par = mean_par,
+    k_or_sigma = k_or_sigma
+  )$value)
   return(integrated_loglik)
 }
