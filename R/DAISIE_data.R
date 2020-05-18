b107cd1e8be6bf3cebd10767386e56a5d367c6d8
luislvalente <luislvalente@gmail.com>
2020-05-18 14:49:13 +0200

    Added archipelagos41 datalist (same as in Nature paper) and corresponding documentation.
    Updated documentation for archipelago_data.

--- R/DAISIE_data.R
+++ R/DAISIE_data.R
@@ -335,11 +335,34 @@ NULL
 NULL
 
 #' @name archipelago_data
-#' @title data on 41 archipelagos
+#' @title Physical data on 41 archipelagos
 #' @docType data
 #' @format A dataframe containing information on archipelago name, area, age and distance from the mainland
 #' @description A dataframe with in subsequent columns the name of the archipelago (Archipelago)
 #' the area of the archipelago (Area), the age (Age) and the distance from the mainland (Distance)
-#' @source Valente et al A simple dynamic model explain the diversty of island birds worldwide. \cr
+#' @source Valente L, Phillimore AB, Melo M, Warren BH, Clegg SM, Havenstein K,
+#'  Tiedemann R, Illera JC, Thébaud C, Aschenbach T, Etienne RS. A simple dynamic model
+#'  explains island bird diversity worldwide (2020) Nature, 579, 92-96 \cr
+#' @keywords datasets
+NULL
+
+
+#' @name archipelagos41
+#' @title DAISIE datalist object including bird phylogenetic data and
+#' physical data for 41 archipelagos
+#' @docType data
+#' @format A datalist containing data on the 41 archipelagos studied in Valente et
+#' al 2020 (Main Dataset D1). Contains colonisation and branching times for bird species
+#' in each of the archipelagos. It also contains information on archipelago name, area,
+#' age and distance from the nearest mainland.
+#' @description A datalist with 41 items representing the 41 archipelagos.
+#' Each archipelago can be called separately using archipelagos41[[x]] with x being a
+#' number between 1 and 41. Using archipelagos41[[x]][[1]] will show just the
+#' top part of the archipelago item where the archipelago name and physical features
+#' are displayed. The structure of each of the archipelagos is the same as regular DAISIE
+#' datalist generated using DAISIE_dataprep.
+#' @source Valente L, Phillimore AB, Melo M, Warren BH, Clegg SM, Havenstein K,
+#'  Tiedemann R, Illera JC, Thébaud C, Aschenbach T, Etienne RS. A simple dynamic model
+#'  explains island bird diversity worldwide (2020) Nature, 579, 92-96 \cr
 #' @keywords datasets
 NULL
