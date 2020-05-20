a4d72684da1db11be3c879eff5214b47550858c2
luislvalente <luislvalente@gmail.com>
2020-05-20 12:01:44 +0200

    minor change

--- tests/testthat/test-DAISIE_MW_ML.R
+++ tests/testthat/test-DAISIE_MW_ML.R
@@ -9,7 +9,7 @@ M19_Nature_parameters<-c(4.007380e-02,  0.000000e+00,  1.945657e+00,  1.504297e-
                           2.936351e-01,  5.909687e-02,  3.826885e-01,  2.651078e-02,
                          -3.653315e+03,  8.000000e+00,  0.000000e+00)
 
-M19_computation<-DAISIE_MW_ML(
+M19_computation<-DAISIE::DAISIE_MW_ML(
     datalist= archipelagos41,
     initparsopt = c(0.040073803,	1.945656546,	0.150429656,	67.25643672,	0.293635061,
                     0.059096872,	0.382688527, 0.026510781),
@@ -27,5 +27,5 @@ M19_computation<-DAISIE_MW_ML(
     distance_dep = 'area_interactive_clado')
 
 
- expect_equal(M19_Nature_parameters,as.numeric(M19_computation),tol=0.000001)
+   testthat::expect_equal(M19_Nature_parameters,as.numeric(M19_computation),tol=0.000001)
 })
