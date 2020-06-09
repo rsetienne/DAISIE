c06a1e39f752997ac82f5a456e1a18d940ec64fe
luislvalente <luislvalente@gmail.com>
2020-06-09 18:48:21 +0200

    automatically generated

---
+++ tests/testthat/test-create_CS_version 2.R
@@ -0,0 +1,71 @@
+context("create_CS_version")
+
+test_that("create_CS_version produces correct output", {
+  CS_version <- create_CS_version(model = 1,
+                                  pick_parameter = NULL,
+                                  distribution = NULL,
+                                  sd = NULL,
+                                  multi_rate_optim_method = NULL)
+  expect_equal(CS_version, list(model = 1,
+                                pick_parameter = NULL,
+                                distribution = NULL,
+                                sd = NULL,
+                                multi_rate_optim_method = NULL))
+
+  CS_version <- create_CS_version(model = 2,
+                                  pick_parameter = "cladogenesis",
+                                  distribution = "gamma",
+                                  sd = 1,
+                                  multi_rate_optim_method = "optimize")
+  expect_equal(CS_version, list(model = 2,
+                                pick_parameter = "cladogenesis",
+                                distribution = "gamma",
+                                sd = 1,
+                                multi_rate_optim_method = "optimize"))
+
+  CS_version <- create_CS_version(model = 3,
+                                  pick_parameter = NULL,
+                                  distribution = NULL,
+                                  sd = NULL,
+                                  multi_rate_optim_method = NULL)
+  expect_equal(CS_version, list(model = 3,
+                                pick_parameter = NULL,
+                                distribution = NULL,
+                                sd = NULL,
+                                multi_rate_optim_method = NULL))
+
+})
+
+
+test_that("abuse create_CS_version", {
+  expect_error(create_CS_version(model = 4,
+                                  pick_parameter = NULL,
+                                  distribution = NULL,
+                                  sd = NULL,
+                                  multi_rate_optim_method = NULL))
+
+  expect_error(create_CS_version(model = 2,
+                                  pick_parameter = NULL,
+                                  distribution = "gamma",
+                                  sd = 1,
+                                  multi_rate_optim_method = "optimize"))
+
+  expect_error(create_CS_version(model = 2,
+                                 pick_parameter = "cladogenesis",
+                                 distribution = NULL,
+                                 sd = 1,
+                                 multi_rate_optim_method = "optimize"))
+
+  expect_error(create_CS_version(model = 2,
+                                 pick_parameter = "cladogenesis",
+                                 distribution = "gamma",
+                                 sd = NULL,
+                                 multi_rate_optim_method = "optimize"))
+
+  expect_error(create_CS_version(model = 2,
+                                 pick_parameter = "cladogenesis",
+                                 distribution = "gamma",
+                                 sd = 1,
+                                 multi_rate_optim_method = NULL))
+})
+
