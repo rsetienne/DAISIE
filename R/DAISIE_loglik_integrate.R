a40b377f99d373718540daad67161a78c86ffd06
joshwlambert <j.w.l.lambert@rug.nl>
2020-07-30 12:29:10 +0200

    added documentation, removed lognormal distribution, removed optimmethod and rearranged DAISIE_loglik_integrate

--- R/DAISIE_loglik_integrate.R
+++ R/DAISIE_loglik_integrate.R
@@ -1,25 +1,8 @@
 #' Integrates the loglikelihood of a single clade across a parameter weighted by a
 #' given distribution
 #'
-#' @inheritParams DAISIE_loglik_CS
-#' @param brts Numeric vector of branching times
-#' @param stac Numeric of Endemicity status
-#' @param missnumspec Number of missing species
-#' @param verbose Logical determining whether output is printed to the concole
-#' @param CS_version a list with the following elements:
-#' \itemize{
-#'   \item{model: the CS model to run, options are \code{1} for single rate
-#'   DAISIE model, \code{2} for multi-rate DAISIE, or \code{0} for IW test
-#'   model}
-#'   \item{pick_parameter: the parameter to relax (integrate over). Options are
-#' \code{"cladogenesis"}, \code{"extinction"}, \code{"carrying_capacity"},
-#' \code{"immigration"}, or \code{"anagenesis"}}
-#'   \item{distribution: the distribution to weigh the likelihood, either
-#' \code{"lognormal"} or \code{"gamma"}}
-#'   \item{sd: standard deviation of the distribution}
-#'   \item{optimmethod: the method used to find the optimum of the integrand,
-#'   either \code{"optimize"} or \code{"subplex"} or \code{"Nelder-Mead"}}
-#'   }
+#' @inheritParams default_params_doc
+#'
 #' @return A loglikelihood value
 DAISIE_loglik_integrate <- function(
   pars1,
@@ -32,68 +15,22 @@ DAISIE_loglik_integrate <- function(
   abstolint,
   reltolint,
   verbose) {
-  testit::assert(is.list(CS_version))
-  if (CS_version$distribution == "lognormal") {
-    rho <- function(DAISIE_par, DAISIE_dist_pars) {
-      sigma_squared <- log(1 + (DAISIE_dist_pars[2] / DAISIE_dist_pars[1])^2)
-      return(stats::dlnorm(x = DAISIE_par,
-                    meanlog = log(DAISIE_dist_pars[1]) - sigma_squared / 2,
-                    sdlog = sqrt(sigma_squared)))
-    }
-  }
-  if (CS_version$distribution == "gamma") {
-    rho <- function(DAISIE_par, DAISIE_dist_pars) {
-      return(stats::dgamma(x = DAISIE_par,
-                           shape = DAISIE_dist_pars[1]^2 / DAISIE_dist_pars[2]^2,
-                           scale = DAISIE_dist_pars[2]^2 / DAISIE_dist_pars[1]))
-    }
-  }
 
+  testit::assert(is.list(CS_version))
   sd <- CS_version$sd
   pick <- which(c("cladogenesis",
                   "extinction",
                   "carrying_capacity",
                   "immigration",
-                  "anagenesis") == CS_version$pick_parameter)
+                  "anagenesis") == CS_version$relaxed_par)
   mean <- pars1[pick]
-
-  DAISIE_loglik_integrand <- function(DAISIE_par,
-                                      pars1,
-                                      pars2,
-                                      brts,
-                                      stac,
-                                      missnumspec,
-                                      methode,
-                                      abstolint,
-                                      reltolint,
-                                      verbose,
-                                      pick,
-                                      mean,
-                                      sd) {
-    pars1[pick] <- DAISIE_par
-    loglik_DAISIE_par <- exp(DAISIE_loglik(
-      pars1 = pars1,
-      pars2 = pars2,
-      brts = brts,
-      stac = stac,
-      missnumspec = missnumspec,
-      methode = methode,
-      abstolint = abstolint,
-      reltolint = reltolint,
-      verbose = verbose)
-    ) *
-      rho(
-        DAISIE_par = DAISIE_par,
-        DAISIE_dist_pars = c(mean, sd)
-      )
-    return(log(loglik_DAISIE_par))
-  }
 
   integrated_loglik <- integral_peak(
     logfun = Vectorize(DAISIE_loglik_integrand,
                        vectorize.args = "DAISIE_par"),
-    optimmethod = CS_version$multi_rate_optim_method,
-    xx = sort(c(seq(-20,20,2),seq(log(mean) - 1,log(mean) + 1),log((mean + 10 * sd)/mean))),
+    xx = sort(c(seq(-20,20,2),
+                seq(log(mean) - 1,log(mean) + 1),
+                log((mean + 10 * sd)/mean))),
     pars1 = pars1,
     pars2 = pars2,
     brts = brts,
@@ -107,8 +44,56 @@ DAISIE_loglik_integrate <- function(
     mean = mean,
     sd = sd
   )
-
   return(integrated_loglik)
+}
+
+#' Gamma distribution density parameterised with mean and standard deviation
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return Numeric
+rho <- function(DAISIE_par, DAISIE_dist_pars) {
+  return(stats::dgamma(x = DAISIE_par,
+                       shape = DAISIE_dist_pars[1]^2 / DAISIE_dist_pars[2]^2,
+                       scale = DAISIE_dist_pars[2]^2 / DAISIE_dist_pars[1]))
+}
+
+#' Integrand to be integrated to calculate the log likelihood for the relaxed
+#' rate model.
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return A numeric
+DAISIE_loglik_integrand <- function(DAISIE_par,
+                                    pars1,
+                                    pars2,
+                                    brts,
+                                    stac,
+                                    missnumspec,
+                                    methode,
+                                    abstolint,
+                                    reltolint,
+                                    verbose,
+                                    pick,
+                                    mean,
+                                    sd) {
+  pars1[pick] <- DAISIE_par
+  loglik_DAISIE_par <- exp(DAISIE_loglik(
+    pars1 = pars1,
+    pars2 = pars2,
+    brts = brts,
+    stac = stac,
+    missnumspec = missnumspec,
+    methode = methode,
+    abstolint = abstolint,
+    reltolint = reltolint,
+    verbose = verbose)
+  ) *
+    rho(
+      DAISIE_par = DAISIE_par,
+      DAISIE_dist_pars = c(mean, sd)
+    )
+  return(log(loglik_DAISIE_par))
 }
 
 #' @title Computes integral of a very peaked function, modified from the SADISA package
@@ -129,7 +114,6 @@ DAISIE_loglik_integrate <- function(
 #' @export
 
 integral_peak <- function(logfun,
-                          optimmethod = 'optimize',
                           xx = seq(-20,20,2),
                           xcutoff = 2,
                           ymaxthreshold = 1E-12,
@@ -137,8 +121,7 @@ integral_peak <- function(logfun,
   fun <- function(x) exp(logfun(x, ...))
   #logQ <- log(stats::integrate(f = fun, lower = 0, upper = Inf, rel.tol = 1e-10, abs.tol = 1e-10)$value)
 
-  # 1/ determine integrand peak
-  if (optimmethod == 'optimize') {
+  # 1 determine integrand peak
     yy <- xx + logfun(exp(xx), ...)
     yy[which(is.na(yy) | is.nan(yy))] <- -Inf
     yymax <- max(yy)
@@ -151,22 +134,24 @@ integral_peak <- function(logfun,
     xlft <- xx[iimax[1]] - xcutoff
     xrgt <- xx[iimax[length(iimax)]] + xcutoff
     optfun <- function(x) x + logfun(exp(x), ...)
-    optres <- stats::optimize(f = optfun, interval = c(xlft,xrgt), maximum = TRUE, tol = 1e-10)
+    optres <- stats::optimize(f = optfun,
+                              interval = c(xlft,xrgt),
+                              maximum = TRUE,
+                              tol = 1e-10)
     xmax <- optres$maximum
     #ymax <- optres$objective
-  } else if (optimmethod == 'subplex') {
-    minfun <- function(x) -exp(logfun(x, ...))
-    optres <- subplex::subplex(par = 1, fn = minfun, control = list(reltol = 1e-10))
-    xmax <- optres$par
-  } else if (optimmethod == 'Nelder-Mead') {
-    minfun <- function(x) -exp(logfun(x, ...))
-    optres <- stats::optim(par = 1, fn = minfun, method = 'Nelder-Mead', control = list(reltol = 1e-10))
-    xmax <- optres$par
-  }
 
-  # 3/ compute integral
-  logQ <- log(stats::integrate(f = fun, lower = 0, upper = exp(xmax), rel.tol = 1e-10, abs.tol = 1e-10)$value +
-          stats::integrate(f = fun, lower = exp(xmax), upper = Inf, rel.tol = 1e-10, abs.tol = 1e-10)$value)
+  # 2 compute integral
+  logQ <- log(stats::integrate(f = fun,
+                               lower = 0,
+                               upper = exp(xmax),
+                               rel.tol = 1e-10,
+                               abs.tol = 1e-10)$value +
+                stats::integrate(f = fun,
+                           lower = exp(xmax),
+                           upper = Inf,
+                           rel.tol = 1e-10,
+                           abs.tol = 1e-10)$value)
 
   #intfun <- function(x) exp((x + logfun(exp(x), ...)) - ymax)
   #corrfact <- stats::integrate(f = intfun, lower = -Inf, upper = xmax, rel.tol = 1e-10, abs.tol = 1e-10)$value +
