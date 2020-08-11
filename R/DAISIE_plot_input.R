ff5a8ad06b01e4b3f773f34aac10a55c1f0a2517
rsetienne <rampaletienne@gmail.com>
2020-08-11 13:01:21 +0200

    Remove stop and add dependency on tibble in example

--- R/DAISIE_plot_input.R
+++ R/DAISIE_plot_input.R
@@ -15,6 +15,10 @@
 #'
 #' @examples
 #'
+#'  # check whether package tibble has been installed
+#'  if (!requireNamespace('tibble', quietly = TRUE)) {
+#'     cat("Package tibble needed for this function to work. Please install it.")
+#'  } else {
 #'  set.seed(42)
 #'
 #'  # Random trees
@@ -54,6 +58,7 @@
 #'    pargs = list(size = 3)
 #'  )
 #' p
+#' }
 #'
 #' @author Raphael Scherrer (github.com/rscherrer)
 #'
@@ -62,6 +67,8 @@ DAISIE_plot_input <- function(
   trees, age = NULL, tcols = NULL, metadata = NULL, mapping = NULL,
   xlen = 0.001, pargs = NULL, bckgd = "white"
 ) {
+  # check whether necessary packages have been installed.
+  needs_installation_of_dependencies <- FALSE
   dependencies <- c(
     "dplyr",
     "ggplot2",
@@ -75,140 +82,144 @@ DAISIE_plot_input <- function(
   )
   for (dependency in dependencies) {
     if (!requireNamespace(dependency, quietly = TRUE)) {
-      stop(paste0(
+      cat(paste0(
         "Package ",
-        dependency , " needed for this function to work. Please install it."),
-        call. = FALSE)
+        dependency ,
+        " is needed for this function to work. Please install it."))
+      needs_installation_of_dependencies <- TRUE
     }
   }
 
-  # lint fix
-  clade <- NULL
-  data <- NULL
-  node <- NULL
-  . <- NULL
-  y <- NULL
-  tcol <- NULL
+  if (!needs_installation_of_dependencies) {
 
-  # Function to stick a layer of white rectangles to hide some parts
-  white_rect <- function(xmin, xmax, ymin, ymax, color = "white") {
-    ggplot2::geom_rect(
-      xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color,
-      fill = color
-    )
-  }
+    # lint fix
+    clade <- NULL
+    data <- NULL
+    node <- NULL
+    . <- NULL
+    y <- NULL
+    tcol <- NULL
 
-  if (is.null(age)) age <- max(purrr::map_dbl(
-    trees, ~ max(ape::node.depth.edgelength(.x))
-  ))
-  if (is.null(tcols)) tcols <- rep(age, length(trees))
+    # Function to stick a layer of white rectangles to hide some parts
+    white_rect <- function(xmin, xmax, ymin, ymax, color = "white") {
+      ggplot2::geom_rect(
+        xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color,
+        fill = color
+      )
+    }
 
-  # Before we add fake tips, identify the actual tips and assign clades
-  tipclades <- list(
-    label = purrr::map(trees, ~ .x$tip.label),
-    clade = purrr::map2(names(trees), trees, ~ rep(.x, length(.y$tip.label)))
-  ) %>% purrr::map_dfc(~ do.call("c", .x))
+    if (is.null(age)) age <- max(purrr::map_dbl(
+      trees, ~ max(ape::node.depth.edgelength(.x))
+    ))
+    if (is.null(tcols)) tcols <- rep(age, length(trees))
 
-  # For each tree...
-  trees <- purrr::map(trees, function(tree) {
+    # Before we add fake tips, identify the actual tips and assign clades
+    tipclades <- list(
+      label = purrr::map(trees, ~ .x$tip.label),
+      clade = purrr::map2(names(trees), trees, ~ rep(.x, length(.y$tip.label)))
+    ) %>% purrr::map_dfc(~ do.call("c", .x))
 
-    # Add an extra branch around the age of the island (this is a hack)
-    crown <- max(ape::node.depth.edgelength(tree))
-    pos <- age - crown
-    tree$root.edge <- pos
-    phytools::bind.tip(
-      tree, "new", edge.length = xlen, where = "root", position = pos
-    )
+    # For each tree...
+    trees <- purrr::map(trees, function(tree) {
 
-  })
+      # Add an extra branch around the age of the island (this is a hack)
+      crown <- max(ape::node.depth.edgelength(tree))
+      pos <- age - crown
+      tree$root.edge <- pos
+      phytools::bind.tip(
+        tree, "new", edge.length = xlen, where = "root", position = pos
+      )
 
-  # Concatenate all the trees together
-  tree <- purrr::reduce(trees, ape::bind.tree)
+    })
 
-  # Total number of tips
-  ntips <- ape::Ntip(tree)
+    # Concatenate all the trees together
+    tree <- purrr::reduce(trees, ape::bind.tree)
 
-  # Plot the meta-tree
-  p <- ggtree::ggtree(tree, ladderize = FALSE)
-  p <- ggtree::revts(p)
-  p <- p + ggtree::theme_tree2()
+    # Total number of tips
+    ntips <- ape::Ntip(tree)
 
-  # Hide the extra tips near the island age (those are here to make sure that
-  # the stems of the different subtrees go all the way to the island age)
-  p <- p + ggplot2::ylim(c(1, ntips))
-  p <- p + white_rect(-age, -age + xlen, 1, ntips, color = bckgd)
+    # Plot the meta-tree
+    p <- ggtree::ggtree(tree, ladderize = FALSE)
+    p <- ggtree::revts(p)
+    p <- p + ggtree::theme_tree2()
 
-  # For each clade, hide the stem up to the colonization event
-  ymax <- 0
-  ymin <- 0
+    # Hide the extra tips near the island age (those are here to make sure that
+    # the stems of the different subtrees go all the way to the island age)
+    p <- p + ggplot2::ylim(c(1, ntips))
+    p <- p + white_rect(-age, -age + xlen, 1, ntips, color = bckgd)
 
-  for (i in seq_along(trees)) {
-    ymin <- ymax + 1
-    ymax <- ymin + ape::Ntip(trees[[i]]) - 1
-    p <- p + white_rect(-age, -tcols[i], ymin, ymax, color = bckgd)
-  }
+    # For each clade, hide the stem up to the colonization event
+    ymax <- 0
+    ymin <- 0
 
-  # Add a dashed line for island emergence
-  p <- p + ggplot2::geom_vline(xintercept = -age, lty = 2)
+    for (i in seq_along(trees)) {
+      ymin <- ymax + 1
+      ymax <- ymin + ape::Ntip(trees[[i]]) - 1
+      p <- p + white_rect(-age, -tcols[i], ymin, ymax, color = bckgd)
+    }
 
-  # Assign clades to tips
-  p$data <- suppressWarnings(p$data %>% dplyr::full_join(tipclades))
+    # Add a dashed line for island emergence
+    p <- p + ggplot2::geom_vline(xintercept = -age, lty = 2)
 
-  # Infer the clades of the internal nodes based on tips
-  nodeclades <- p$data %>%
-    dplyr::group_by(clade) %>%
-    tidyr::nest() %>%
-    dplyr::filter(!is.na(clade)) %>%
-    dplyr::mutate(mrca = purrr::map_if(
-      data,
-      ~ nrow(.x) > 1,
-      ~ ape::getMRCA(tree, .x$label),
-      .else = ~ .x$node[1]
-    ) %>% unlist) %>%
-    dplyr::mutate(node = purrr::map2(
-      data,
-      mrca,
-      function(x, y) {
-        out <- y
-        if (nrow(x) > 1) out <- c(out, tidytree::offspring(tree, y))
-        out
-      }
-    )) %>%
-    dplyr::select(-data) %>%
-    tidyr::unnest(cols = c(node)) %>%
-    .[order(.$node),]
+    # Assign clades to tips
+    p$data <- suppressWarnings(p$data %>% dplyr::full_join(tipclades))
 
-  p$data$clade[p$data$node %in% nodeclades$node] <- nodeclades$clade
-  p$data$mrca <- NA
-  p$data$mrca[p$data$node %in% nodeclades$node] <- nodeclades$mrca
-
-  # Assign clades with optional extra data
-  if (!is.null(metadata)) {
-    metadata <- metadata %>% tibble::add_row(clade = NA)
-    p$data <- p$data %>%
+    # Infer the clades of the internal nodes based on tips
+    nodeclades <- p$data %>%
       dplyr::group_by(clade) %>%
       tidyr::nest() %>%
-      dplyr::right_join(metadata, by = "clade") %>%
-      tidyr::unnest(cols = c(data))
+      dplyr::filter(!is.na(clade)) %>%
+      dplyr::mutate(mrca = purrr::map_if(
+        data,
+        ~ nrow(.x) > 1,
+        ~ ape::getMRCA(tree, .x$label),
+        .else = ~ .x$node[1]
+      ) %>% unlist) %>%
+      dplyr::mutate(node = purrr::map2(
+        data,
+        mrca,
+        function(x, y) {
+          out <- y
+          if (nrow(x) > 1) out <- c(out, tidytree::offspring(tree, y))
+          out
+        }
+      )) %>%
+      dplyr::select(-data) %>%
+      tidyr::unnest(cols = c(node)) %>%
+      .[order(.$node),]
+
+    p$data$clade[p$data$node %in% nodeclades$node] <- nodeclades$clade
+    p$data$mrca <- NA
+    p$data$mrca[p$data$node %in% nodeclades$node] <- nodeclades$mrca
+
+    # Assign clades with optional extra data
+    if (!is.null(metadata)) {
+      metadata <- metadata %>% tibble::add_row(clade = NA)
+      p$data <- p$data %>%
+        dplyr::group_by(clade) %>%
+        tidyr::nest() %>%
+        dplyr::right_join(metadata, by = "clade") %>%
+        tidyr::unnest(cols = c(data))
+    }
+
+    # Apply aesthetic mapping
+    if (!is.null(mapping)) p <- p + mapping
+
+    # Add points at colonization events
+    mrca <- p$data %>%
+      dplyr::filter(!is.na(clade)) %>%
+      dplyr::group_by(clade, mrca) %>%
+      dplyr::summarize(y = y[node == mrca]) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(tcol = tcols)
+
+    if (!is.null(metadata)) mrca <- mrca %>%
+      dplyr::right_join(metadata, by = "clade")
+
+    pargs <- c(list(data = mrca, mapping = ggplot2::aes(x = -tcol, y = y)), pargs)
+    geom_point_ <- ggplot2::geom_point
+    p <- p + do.call("geom_point_", pargs)
+
+    return(p)
   }
-
-  # Apply aesthetic mapping
-  if (!is.null(mapping)) p <- p + mapping
-
-  # Add points at colonization events
-  mrca <- p$data %>%
-    dplyr::filter(!is.na(clade)) %>%
-    dplyr::group_by(clade, mrca) %>%
-    dplyr::summarize(y = y[node == mrca]) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(tcol = tcols)
-
-  if (!is.null(metadata)) mrca <- mrca %>%
-    dplyr::right_join(metadata, by = "clade")
-
-  pargs <- c(list(data = mrca, mapping = ggplot2::aes(x = -tcol, y = y)), pargs)
-  geom_point_ <- ggplot2::geom_point
-  p <- p + do.call("geom_point_", pargs)
-
-  p
 }
