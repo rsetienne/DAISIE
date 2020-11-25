0934f6a8d7435cf8b4c33ee010e98d97b1044329
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-25 23:57:31 +0000

    Add $all_colonisations reference tests. Progress #125

--- tests/testthat/test-DAISIE_ONEcolonist.R
+++ tests/testthat/test-DAISIE_ONEcolonist.R
@@ -238,10 +238,16 @@ test_that("DAISIE_ONEcolonist stac and brts works for 1 nonendemic colonist", {
     result$branching_times,
     c(sim_time, max(as.numeric(island_spec[, 3])))
   )
+
   # Stac 3 for recolonisation cases
   expect_equal(result$stac, 3)
 
-  #TODO: all_clades
+  expect_equal(result$all_colonisations[[1]]$event_times, c(2, 0.7))
+  expect_equal(result$all_colonisations[[1]]$species_type, "A")
+
+  expect_equal(result$all_colonisations[[2]]$event_times, c(2, 0.6))
+  expect_equal(result$all_colonisations[[2]]$species_type, "I")
+
 })
 
 test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic colonists", {
@@ -280,7 +286,14 @@ test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic colonists", {
   # stac 3 for recolonisation cases
   expect_equal(result$stac, 3)
 
-  #TODO: all_clades
+  expect_equal(result$all_colonisations[[1]]$event_times, c(2, 0.7))
+  expect_equal(result$all_colonisations[[1]]$species_type, "A")
+
+  expect_equal(result$all_colonisations[[2]]$event_times, c(2, 0.6))
+  expect_equal(result$all_colonisations[[2]]$species_type, "A")
+
+  expect_equal(result$all_colonisations[[3]]$event_times, c(2, 0.5))
+  expect_equal(result$all_colonisations[[3]]$species_type, "I")
 })
 
 test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic clades", {
@@ -288,7 +301,7 @@ test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic clades", {
 
   sim_time <- 2
 
-  #
+
   # Species Mainland Ancestor Colonisation time (BP) Species type branch_code branching time (BP) Anagenetic_origin
   # [1,] "4"     "1"               "1.13468671408026"     "C"          "AA"        "1.13468671408026"  NA
   # [2,] "3"     "1"               "1.13468671408026"     "C"          "B"         "0.96545899791969"  NA
@@ -339,33 +352,93 @@ test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic clades", {
   # stac 3 for recolonisation cases
   expect_equal(result$stac, 3)
 
-  #TODO: all_clades
+  # all_colonisations
+  expect_equal(result$all_colonisations[[1]]$event_times, c(
+    2.0,
+    1.13468671408026,
+    0.96545899791969,
+    0.68696590746724
+  ))
+  expect_equal(result$all_colonisations[[1]]$species_type, "C")
+
+  expect_equal(result$all_colonisations[[2]]$event_times, c(
+    2.0,
+    0.67395467208331,
+    0.34198900695798
+  ))
+  expect_equal(result$all_colonisations[[2]]$species_type, "C")
 })
 
+test_that("DAISIE_ONEcolonist stac and brts works for 2 endemic clades", {
+  # With > 1 endemic clades, function works
 
-# sim_time <- 2
-# n_mainland_species <- 1
-# clado_rate <- 5
-# ext_rate <- 0.1
-# carr_cap <- 6
-# imm_rate <- 2.0
-# ana_rate <- 0
-# set.seed(3)
-# area_pars <- DAISIE::create_area_pars(
-#   max_area = 1,
-#   current_area = 1,
-#   proportional_peak_t = 0,
-#   total_island_age = 0,
-#   sea_level_amplitude = 0,
-#   sea_level_frequency = 0,
-#   island_gradient_angle = 0)
-# hyper_pars <- create_hyper_pars(d = 0, x = 0)
-# nonoceanic_pars <- c(0, 0)
-# sim <- DAISIE:::DAISIE_sim_core_constant_rate(
-#   time = sim_time,
-#   mainland_n = n_mainland_species,
-#   pars = c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate),
-#   area_pars = area_pars,
-#   hyper_pars = hyper_pars,
-#   nonoceanic_pars = nonoceanic_pars)
+  sim_time <- 2
+
+
+  # Species Mainland Ancestor Colonisation time (BP) Species type branch_code branching time (BP) Anagenetic_origin
+  # [1,] "4"     "1"               "1.13468671408026"     "C"          "AA"        "1.13468671408026"  NA
+  # [2,] "3"     "1"               "1.13468671408026"     "C"          "B"         "0.96545899791969"  NA
+  # [3,] "5"     "1"               "1.13468671408026"     "C"          "AB"        "0.68696590746724"  NA
+  # [4,] "6"     "1"               "0.67395467208331"     "C"          "A"         "0.67395467208331"  NA
+  # [5,] "7"     "1"               "0.67395467208331"     "C"          "B"         "0.34198900695798"  NA
+
+
+  island_spec <- matrix(nrow = 5, ncol = 7, data = "x")
+  island_spec[, 1] <- c("4", "3", "5", "6", "7")
+  island_spec[, 2] <- c("1", "1", "1", "1", "1")
+  island_spec[, 3] <-
+    c("1.13468671408026",
+      "1.13468671408026",
+      "1.13468671408026",
+      "0.67395467208331",
+      "0.67395467208331")
+  island_spec[, 4] <- c("C", "C", "C", "C", "C")
+  island_spec[, 5] <- c("AA", "B", "AB", "A", "B")
+  island_spec[, 6] <-
+    c(1.13468671408026,
+      0.96545899791969,
+      0.68696590746724,
+      0.67395467208331,
+      0.34198900695798)
+  island_spec[, 7] <- c(NA, NA, NA, NA, NA)
+  colnames(island_spec) <- c(
+    "Species",
+    "Mainland Ancestor",
+    "Colonisation time (BP)",
+    "Species type",
+    "branch_code",
+    "branching time (BP)",
+    "Anagenetic_origin"
+  )
+  stt_table <- NULL
+  result <- DAISIE:::DAISIE_ONEcolonist(
+    time = sim_time,
+    island_spec = island_spec,
+    stt_table = stt_table
+  )
+  btimes <- sort(as.numeric(island_spec[, 6]), decreasing = TRUE)
+  btimes_sans_yng_col <- btimes[-4]
+  expect_equal(
+    result$branching_times,
+    c(sim_time, btimes_sans_yng_col)
+  )
+  # stac 3 for recolonisation cases
+  expect_equal(result$stac, 3)
+
+  # all_colonisations
+  expect_equal(result$all_colonisations[[1]]$event_times, c(
+    2.0,
+    1.13468671408026,
+    0.96545899791969,
+    0.68696590746724
+  ))
+  expect_equal(result$all_colonisations[[1]]$species_type, "C")
+
+  expect_equal(result$all_colonisations[[2]]$event_times, c(
+    2.0,
+    0.67395467208331,
+    0.34198900695798
+  ))
+  expect_equal(result$all_colonisations[[2]]$species_type, "C")
+})
 
