c3c767e8d0ab8ab815e2d67bbbd0e4a46c0997d6
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-28 01:03:17 +0000

    Remove get_brts_mya(). Progress #126

--- tests/testthat/test-DAISIE_get_brts_mya.R
+++
@@ -1,9 +0,0 @@
-context("DAISIE_get_brts_mya")
-
-test_that("use", {
-  data(Galapagos_datatable)
-  branching_times_mya <- DAISIE_get_brts_mya(data_table = Galapagos_datatable)
-  expect_true(class(branching_times_mya) == "numeric")
-  expect_true(length(branching_times_mya) > 1)
-  expect_true(all(branching_times_mya > 0))
-})
