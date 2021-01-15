f32946c61aff2c2a0948430762d7bde3048d66ff
= <=>
2021-01-15 17:58:48 +0100

    invoke_IW to test

--- tests/testthat/test-integration_DAISIE.R
+++ tests/testthat/test-integration_DAISIE.R
@@ -275,3 +275,31 @@ test_that("conditioning works", {
   )
   testthat::expect_equal(loglik_CS_2type_cond5,-61.5667762281177673)
 })
+
+test_that("various solver options give similar results", {
+  IW <- function(methode = 'odeint::runge_kutta_fehlberg78')
+  {
+    utils::data(frogs_datalist, package = "DAISIE")
+    pars1 <- c(0.2, 0.1, 1000.1, 0.001, 0.3)
+    pars2 <- c(40, 11, 0, 0)
+    loglik_IW <- DAISIE::DAISIE_loglik_IW(
+      pars1 = pars1,
+      pars2 = pars2,
+      datalist = frogs_datalist,
+      methode = methode,
+      abstolint = 1E-12,
+      reltolint = 1E-10,
+    )
+    return(loglik_IW)
+  }
+  IW0 <- IW()
+  IW1 <- IW(methode = 'odeint::runge_kutta_cash_karp54')
+  IW2 <- IW(methode = 'odeint::runge_kutta_dopri5')
+  IW3 <- IW(methode = 'odeint::bulirsch_stoer')
+  IW4 <- IW(methode = 'ode45')
+  #print(c(IW0,IW1,IW2,IW3,IW4))
+  testthat::expect_equal(IW0,IW1, tolerance = 1E-4)
+  testthat::expect_equal(IW0,IW2, tolerance = 1E-4)
+  testthat::expect_equal(IW0,IW3, tolerance = 1E-4)
+  testthat::expect_equal(IW0,IW4, tolerance = 1E-4)
+})
