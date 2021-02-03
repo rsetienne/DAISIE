6022a5d7dbab3ff8ae4ba9b2283440c10a1851c3
Neves-P <p.m.santos.neves@rug.nl>
2021-02-03 23:43:38 +0100

    skip_on_covr() and remove #nocov flags

--- tests/testthat/test-DAISIE_loglik_IW_solver.R
+++ tests/testthat/test-DAISIE_loglik_IW_solver.R
@@ -1,8 +1,9 @@
 test_that("loglik IW various solver options give similar results", {
   # Test is not included in coverage due to issue with running loglik_IW
   # code from covr::package_coverage()
+  testthat::skip_on_covr("Fails on covr")
 
-  utils::data(frogs_datalist, package = "DAISIE") #nocov start
+  utils::data(frogs_datalist, package = "DAISIE")
   pars1 <- c(0.2, 0.1, 1000.1, 0.001, 0.3)
   pars2 <- c(40, 11, 0, 0)
 
@@ -51,5 +52,5 @@ test_that("loglik IW various solver options give similar results", {
   testthat::expect_equal(IW0,IW1, tolerance = 1E-4)
   testthat::expect_equal(IW0,IW2, tolerance = 1E-4)
   testthat::expect_equal(IW0,IW3, tolerance = 1E-4)
-  testthat::expect_equal(IW0,IW4, tolerance = 1E-4) #nocov end
+  testthat::expect_equal(IW0,IW4, tolerance = 1E-4)
 })
