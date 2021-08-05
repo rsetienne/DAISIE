9c7918864525be6b403d3293730bf1586dcb70cc
Hanno <h.hildenbrandt@rug.nl>
2021-08-05 13:52:19 +0200

    DAISIE_IW_num_threads
    Signed-off-by: Hanno <h.hildenbrandt@rug.nl>

--- src/R_init_DAISIE.c
+++ src/R_init_DAISIE.c
@@ -19,10 +19,12 @@ static const R_FortranMethodDef FortranEntries[] = {
 
 
 /* C bindings */
+extern SEXP daisie_odeint_iw_num_threads(SEXP);
 extern SEXP daisie_odeint_iw(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP daisie_odeint_cs(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
+  {"daisie_odeint_iw_num_threads", (DL_FUNC) &daisie_odeint_iw_num_threads, 1},
   {"daisie_odeint_iw", (DL_FUNC) &daisie_odeint_iw, 6},
   {"daisie_odeint_cs", (DL_FUNC) &daisie_odeint_cs, 9},
   {NULL, NULL, 0}
