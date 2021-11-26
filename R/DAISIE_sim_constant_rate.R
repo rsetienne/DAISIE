57cb5713cb87247c1915b7f5aab27a0f74688edc
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-11-26 12:42:48 +0100

    added simplified DAISIE_sim_constant_rate added DAISIE_sim_constant_rate_cs, DAISIE_sim_constant_rate_iw, and DAISIE_sim_constant_rate_gw

--- R/DAISIE_sim_constant_rate.R
+++ R/DAISIE_sim_constant_rate.R
@@ -200,180 +200,47 @@ DAISIE_sim_constant_rate <- DAISIE_sim <- function(
   testit::assert(are_hyper_pars(hyper_pars = hyper_pars))
   testit::assert(are_area_pars(area_pars = area_pars))
 
-  totaltime <- time
-  island_replicates <- list()
-    if (divdepmodel == "IW") {
-      for (rep in 1:replicates) {
-        if (cond == 0) {
-          number_present <- -1
-        } else {
-          number_present <- 0
-        }
-        while (number_present < cond) {
-          island_replicates[[rep]] <- DAISIE_sim_core_constant_rate(
-            time = totaltime,
-            mainland_n = M,
-            pars = pars,
-            nonoceanic_pars = nonoceanic_pars,
-            hyper_pars = hyper_pars,
-            area_pars = area_pars
-          )
-          stac_vec <- unlist(island_replicates)[which(names(unlist(island_replicates)) == "stac")]
-          present <- which(stac_vec != 0)
-          number_present <- length(present)
-        }
-        if (verbose == TRUE) {
-          print(paste("Island replicate ", rep, sep = ""))
-        }
-      }
-      island_replicates <- DAISIE_format_IW(
-        island_replicates = island_replicates,
-        time = totaltime,
-        M = M,
-        sample_freq = sample_freq,
-        verbose = verbose)
-    }
-    if (divdepmodel == "CS") {
-      if (length(pars) == 5) {
-        for (rep in 1:replicates) {
-          island_replicates[[rep]] <- list()
-          full_list <- list()
-          if (cond == 0) {
-            number_present <- -1
-          } else {
-            number_present <- 0
-          }
-          while (number_present < cond) {
-            for (m_spec in 1:M) {
-              full_list[[m_spec]] <- DAISIE_sim_core_constant_rate(
-                time = totaltime,
-                mainland_n = 1,
-                pars = pars,
-                nonoceanic_pars = nonoceanic_pars,
-                hyper_pars = hyper_pars,
-                area_pars = area_pars
-              )
-            }
-            stac_vec <- unlist(full_list)[which(names(unlist(full_list)) == "stac")]
-            present <- which(stac_vec != 0)
-            number_present <- length(present)
-          }
-          island_replicates[[rep]] <- full_list
-          if (verbose == TRUE) {
-            print(paste("Island replicate ", rep, sep = ""))
-          }
-        }
-      } else if (length(pars) == 10) {
-        if (cond > 0) {
-          warning(
-            paste0(
-              "Conditioning on number of colonisations is not implemented for 2
-  type simulations. Returning result with no conditioning instead."
-            )
-          )
-        }
-        if (replicates_apply_type2 == TRUE) {
-          island_replicates <- DAISIE_sim_min_type2(
-          time = totaltime,
-          M = M,
-          pars = pars,
-          replicates = replicates,
-          prop_type2_pool = prop_type2_pool,
-          area_pars = area_pars,
-          hyper_pars = hyper_pars,
-          verbose = verbose)
-      } else {
-        for (rep in 1:replicates) {
-          pool2 <- DDD::roundn(M * prop_type2_pool)
-          pool1 <- M - pool2
-          lac_1 <- pars[1]
-          mu_1 <- pars[2]
-          K_1 <- pars[3]
-          gam_1 <- pars[4]
-          laa_1 <- pars[5]
-          lac_2 <- pars[6]
-          mu_2 <- pars[7]
-          K_2 <- pars[8]
-          gam_2 <- pars[9]
-          laa_2 <- pars[10]
-          full_list <- list()
-          #### species of pool1
-          for (m_spec in 1:pool1) {
-            full_list[[m_spec]] <- DAISIE_sim_core_constant_rate(
-              time = totaltime,
-              mainland_n = 1,
-              pars = c(lac_1,
-                       mu_1,
-                       K_1,
-                       gam_1,
-                       laa_1),
-              area_pars = area_pars,
-              hyper_pars = hyper_pars,
-              nonoceanic_pars = c(0, 0))
-            full_list[[m_spec]]$type1or2  <- 1
-          }
-          #### species of pool2
-          for (m_spec in (pool1 + 1):(pool1 + pool2)) {
-            full_list[[m_spec]] <- DAISIE_sim_core_constant_rate(
-              time = totaltime,
-              mainland_n = 1,
-              pars = c(lac_2,
-                       mu_2,
-                       K_2,
-                       gam_2,
-                       laa_2),
-              area_pars = area_pars,
-              hyper_pars = hyper_pars,
-              nonoceanic_pars = c(0, 0))
-            full_list[[m_spec]]$type1or2 <- 2
-          }
-          island_replicates[[rep]] <- full_list
-          if (verbose == TRUE) {
-            print(paste("Island replicate ", rep, sep = ""))
-          }
-        }
-      }
-    }
-    island_replicates <- DAISIE_format_CS(
-      island_replicates = island_replicates,
-      time = totaltime,
-      M = M,
-      sample_freq = sample_freq,
-      verbose = verbose
-    )
-  }
+  total_time <- time
 
-  if (divdepmodel == "GW") {
-    if (!is.numeric(num_guilds)) {
-      stop("num_guilds must be numeric")
-    }
-    guild_size <- M / num_guilds
-    testit::assert(num_guilds < M)
-    testit::assert(M %% num_guilds == 0)
-    for (rep in 1:replicates) {
-      island_replicates[[rep]] <- list()
-      full_list <- list()
-      for (m_spec in 1:num_guilds) {
-        full_list[[m_spec]]  <- DAISIE_sim_core_constant_rate(
-          time = totaltime,
-          mainland_n = guild_size,
-          pars = pars,
-          nonoceanic_pars = nonoceanic_pars,
-          hyper_pars = hyper_pars,
-          area_pars = area_pars
-        )
-      }
-      island_replicates[[rep]] <- full_list
-      if (verbose == TRUE) {
-        print(paste("Island replicate ", rep, sep = ""))
-      }
-    }
-    island_replicates <- DAISIE_format_GW(
-      island_replicates = island_replicates,
-      time = totaltime,
+  if (divdepmodel == "IW") {
+    island_replicates <- DAISIE_sim_constant_rate_iw(
+      total_time = total_time,
       M = M,
+      pars = pars,
+      replicates = replicates,
+      nonoceanic_pars = nonoceanic_pars,
       sample_freq = sample_freq,
+      hyper_pars = hyper_pars,
+      area_pars = area_pars,
+      cond = cond,
+      verbose = verbose)
+  }
+  if (divdepmodel == "CS") {
+    island_replicates <- DAISIE_sim_constant_rate_cs(
+      total_time = total_time,
+      M = M,
+      pars = pars,
+      replicates = replicates,
+      nonoceanic_pars = nonoceanic_pars,
+      prop_type2_pool = prop_type2_pool,
+      replicates_apply_type2 = replicates_apply_type2,
+      sample_freq = sample_freq,
+      hyper_pars = hyper_pars,
+      area_pars = area_pars,
+      cond = cond,
+      verbose = verbose)
+  }
+  if (divdepmodel == "GW") {
+    island_replicates <- DAISIE_sim_constant_rate_gw(
+      total_time = total_time,
+      M = M,
+      pars = pars,
+      replicates = replicates,
+      nonoceanic_pars = nonoceanic_pars,
       num_guilds = num_guilds,
+      sample_freq = sample_freq,
+      hyper_pars = hyper_pars,
+      area_pars = area_pars,
       verbose = verbose)
   }
   if (plot_sims == TRUE) {
