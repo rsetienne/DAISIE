d930b7ee4160ca2eb4498882d4e2b4e7f9ef5968
rsetienne <rampaletienne@gmail.om>
2022-01-28 15:21:15 +0100

    Undo last commit to allow for 6 parameters

--- R/DAISIE_loglik_CS.R
+++ R/DAISIE_loglik_CS.R
@@ -808,7 +808,7 @@ DAISIE_loglik_CS <- DAISIE_loglik_all <- function(
   cond <- pars2[3]
   endpars1 <- 5
 
-  if(length(pars1) == 5 | length(pars1) == 6 | !is.na(pars2[5])) {
+  if(length(pars1) == 5 | !is.na(pars2[5])) {
     if(!is.na(pars2[5]))
     {
       endpars1 <- length(pars1)
