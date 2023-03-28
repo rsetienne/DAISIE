129e0bdecfe7f21041a58f6b07e26cf646ec6c9f
Neves-P <p.m.santos.neves@rug.nl>
2023-03-28 18:36:33 +0200

    Add printing tests

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -1,4 +1,4 @@
-test_that("printing is done", {
+test_that("print_parameters_and_loglik works", {
   expect_message(
     print_parameters_and_loglik(
       pars = c(1, 2:6),
@@ -16,4 +16,74 @@ test_that("printing is done", {
 
   expect_silent(print_parameters_and_loglik(pars = data.frame(rbind(c(2:6),c(12:16))), loglik = -3, verbose = 1, type = 'multiple_island_ML'))
   expect_silent(print_parameters_and_loglik(pars = data.frame(rbind(c(2:6),c(12:16))), loglik = -3, verbose = 0, type = 'multiple_island_ML'))
+})
+
+test_that("print_ml_par_settings works", {
+
+  expect_message(
+    print_ml_par_settings(
+      namepars = letters[1:5],
+      idparsopt = 1:5,
+      idparsfix = NULL,
+      idparsnoshift = 1:5,
+      all_no_shift = 1:5,
+      verbose = 1
+    ),
+    regexp = "You are optimizing: a b c d e
+You are fixing: nothing")
+
+  expect_message(
+    print_ml_par_settings(
+      namepars = letters[1:5],
+      idparsopt = 1:4,
+      idparsfix = 5,
+      idparsnoshift = 1:5,
+      all_no_shift = 1:5,
+      verbose = 1
+    ),
+    regexp = "You are optimizing: a b c d
+You are fixing: e")
+
+  expect_message(
+    print_ml_par_settings(
+      namepars = letters[1:5],
+      idparsopt = 1:5,
+      idparsfix = NULL,
+      idparsnoshift = 1:4,
+      all_no_shift = 1:4,
+      verbose = 1
+    ),
+    regexp = "You are optimizing: a b c d e
+You are fixing: nothing
+You are not shifting: a b c d")
+
+  expect_silent(
+    print_ml_par_settings(
+      namepars = letters[1:5],
+      idparsopt = 1:5,
+      idparsfix = NULL,
+      idparsnoshift = 1:4,
+      all_no_shift = 1:4,
+      verbose = 0
+    )
+  )
+
+})
+
+test_that("print_init_ll works", {
+  expect_message(
+    print_init_ll(
+      initloglik = 10,
+      verbose = 1
+    ),
+    regexp = "Calculating the likelihood for the initial parameters.
+The loglikelihood for the initial parameter values is 10
+Optimizing the likelihood - this may take a while.")
+
+  expect_silent(
+    print_init_ll(
+      initloglik = 10,
+      verbose = 0
+    )
+  )
 })
