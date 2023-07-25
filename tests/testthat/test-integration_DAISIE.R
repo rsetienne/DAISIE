345c77bcfce691b732827e653ddaafa1179ecd0e
Neves-P <p.m.santos.neves@rug.nl>
2023-07-26 01:03:51 +0200

    Silence for debugging [run ci]

--- tests/testthat/test-integration_DAISIE.R
+++ tests/testthat/test-integration_DAISIE.R
@@ -201,7 +201,8 @@ test_that("DAISIE_ML simple case works with nonzero probability of initial
     idparsopt = 1:5,
     parsfix = 0.1,
     idparsfix = 6,
-    verbose = 3,
+    verbose = 0,
+    # verbose = 3,
     methode = "odeint::bulirsch_stoer"
   )
 # )
