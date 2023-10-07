dfc99b7223e800dedcc4f80affb22e8075b0a2ad
Neves-P <p.m.santos.neves@rug.nl>
2023-10-07 20:51:13 +0200

    lsodes fortran [run ci]

--- tests/testthat/test-integration_DAISIE.R
+++ tests/testthat/test-integration_DAISIE.R
@@ -203,7 +203,7 @@ test_that("DAISIE_ML simple case works with nonzero probability of initial
               idparsfix = 6,
               verbose = 0,
               # verbose = 3,
-              methode = "odeint::runge_kutta_fehlberg78"
+              methode = "lsodes"
             )
             # )
 
