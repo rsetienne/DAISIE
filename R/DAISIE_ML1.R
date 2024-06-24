3f9cf6dfb0b222c1963e3ce38d7f730a469f058c
rsetienne <rampaletienne@gmail.om>
2024-06-24 14:53:46 +0200

    Undo previous commit

--- R/DAISIE_ML1.R
+++ R/DAISIE_ML1.R
@@ -214,9 +214,11 @@ DAISIE_ML1 <- function(
       "lambda_a2",
       "prop_type2"
     )
+    nc <- NA
+    names(nc) <- "prob_init_pres"
     out2err <- add_column_to_dataframe(df = out2err,
                                        position = 'lambda_a',
-                                       column_to_insert = NA)
+                                       column_to_insert = nc)
   }
 
   print_ml_par_settings(
