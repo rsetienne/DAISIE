18ddc2e0eb708af153339d9732d0946935394aa9
rsetienne <rampaletienne@gmail.om>
2024-07-01 09:25:23 +0200

    Update test DAISIE_ML1

--- tests/testthat/test-DAISIE_ML1.R
+++ tests/testthat/test-DAISIE_ML1.R
@@ -1,6 +1,6 @@
 context("DAISIE_ML1")
 
-test_that("use", {
+test_that("DAISIE_ML1 works and simplex and subplex give the same answer", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   skip_on_cran()
   data(Galapagos_datalist)
@@ -10,7 +10,7 @@ test_that("use", {
   idparsopt <- c(1,2,3,4,5)
   parsfix <- c()
   idparsfix <- c()
-    tested_MLE <- DAISIE_ML1(
+    tested_MLE1 <- DAISIE_ML1(
       datalist = datalist,
       initparsopt = initparsopt,
       idparsopt = idparsopt,
@@ -18,21 +18,35 @@ test_that("use", {
       ddmodel = ddmodel,
       idparsfix = idparsfix,
       verbose = 0,
-      tol = c(0.01, 0.1, 0.001),
+      tol = c(0.0001, 0.001, 0.0001),
+      res = 15,
+      tolint = c(0.1, 0.01),
+      optimmethod = 'subplex',
+      num_cycles = 3)
+    tested_MLE2 <- DAISIE_ML1(
+      datalist = datalist,
+      initparsopt = as.numeric(tested_MLE1[1:5]),
+      idparsopt = idparsopt,
+      parsfix = parsfix,
+      ddmodel = ddmodel,
+      idparsfix = idparsfix,
+      verbose = 0,
+      tol = c(0.0001, 0.001, 0.0001),
       res = 15,
       tolint = c(0.1, 0.01),
       optimmethod = 'simplex')
+  testthat::expect_equal(tested_MLE1, tested_MLE2)
   expected_MLE <- data.frame(
-    lambda_c = 3.689104200780465,
-    mu = 4.31030299415995,
-    K = 906.6501180193454,
-    gamma = 0.0173458887696076,
-    lambda_a = 3.677789527566334,
-    loglik = -64.2199684450019,
+    lambda_c = 4.0275356252375420,
+    mu = 4.8740259531255852,
+    K = 2001.9278290878507960,
+    gamma = 0.0202054262850127,
+    lambda_a = 4.0124628182465916,
+    loglik = -63.9341963365704231,
     df = 5L,
     conv = 0L
   )
-  testthat::expect_equal(tested_MLE, expected_MLE)
+  testthat::expect_equal(tested_MLE1, expected_MLE)
 })
 
 test_that("abuse", {
