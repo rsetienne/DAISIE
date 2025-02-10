0d2602da47f655c953308e9c875ebecdcb701d80
rsetienne <rampaletienne@gmail.om>
2025-02-10 13:06:32 +0100

    Update pars2

--- tests/testthat/test-DAISIE_loglik_CS.R
+++ tests/testthat/test-DAISIE_loglik_CS.R
@@ -2,8 +2,7 @@ test_that("DAISIE_loglik_CS_choice produces correnict output for CS_version 1", 
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   skip_on_cran()
   pars1 <- c(2.000, 2.700, 20.000, 0.009, 1.010)
-  pars2 <- c(1.0e+02, 1.1e+01, 0.0e+00, 0.0e+00, NA, 0.0e+00, 1.0e-04,
-             1.0e-05, 1.0e-07, 3.0e+03, 9.5e-01, 9.8e-01)
+  pars2 <- c(1.0e+02, 1.1e+01, 0.0e+00, 0.0e+00, NA)
   brts <- c(4.0000, 3.0282, 1.3227, 0.8223, 0.4286, 0.3462, 0.2450, 0.0808,
             0.0527, 0.0327, 0.0221, 0.1180, 0.0756, 0.0525, 0.0322, 0.0118)
   stac <- 2
