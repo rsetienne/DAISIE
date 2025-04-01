26ec1d83e3d22fb2ad9b27b37d264c1626ac6848
rsetienne <rampaletienne@gmail.om>
2025-04-01 11:20:27 +0200

    missing species warning

--- R/DAISIE_ML1.R
+++ R/DAISIE_ML1.R
@@ -256,9 +256,9 @@ DAISIE_ML1 <- function(
     return(out2err)
   }
 
-  if (max(missnumspec) > res/10) {
+  if (max(missnumspec) > res/10 && max(missnumspec) <= (res - 1)) {
     warning(
-      "The number of missing species is quite low relative to the
+      "The number of missing species is quite high relative to the
         resolution of the ODE.")
   }
 
