b0233132f46d44094cff78535e480c25ce13e89f
O. Dehayem Nanwou <o.d.nanwou@rug.nl>
2025-04-15 18:10:29 +0200

    update function

--- R/DAISIE_DE_loglik_all_choosepar.R
+++ R/DAISIE_DE_loglik_all_choosepar.R
@@ -3,6 +3,7 @@
 ### function to calculate the log likelihood of all lineages in an island dataset
 ###############################################################################
 
+
 DAISIE_DE_loglik_all_choosepar <- function(trparsopt,
                                            trparsfix,
                                            idparsopt,
