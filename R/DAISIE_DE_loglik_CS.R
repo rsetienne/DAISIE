0e1ae9a0c7ca86507edd894efedd086a629514bd
rsetienne <rampaletienne@gmail.om>
2025-04-23 16:48:36 +0200

    Fix merge bug

--- R/DAISIE_DE_loglik_CS.R
+++ R/DAISIE_DE_loglik_CS.R
@@ -50,6 +50,37 @@ DAISIE_DE_loglik_CS <- function( pars1,pars2,datalist,
 
   for (i in 2:length(datalist)) {
 
+    if (stac == 1) {
+      loglikelihood <- DAISIE_DE_logpNE_max_age_coltime(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 2) {
+      if (length(brts) == 2)
+        loglikelihood <- DAISIE_DE_logpES(datalist,i,pars1,methode,reltolint,abstolint)
+      else
+        loglikelihood <- DAISIE_DE_logpEC(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 3) {
+      if (length(brts) == 2)
+        loglikelihood <- DAISIE_DE_logpES_mainland(datalist,i,pars1,methode,reltolint,abstolint)
+      else
+        loglikelihood <- DAISIE_DE_logpEC_mainland(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 4) {
+      loglikelihood <- DAISIE_DE_logpNE(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 5) {
+      loglikelihood <- DAISIE_DE_logpES_max_age_coltime(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 6) {
+      loglikelihood <- DAISIE_DE_logpEC_max_age_coltime(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 7) {
+      if (length(brts) == 2)
+        loglikelihood <- DAISIE_DE_logpES_max_age_coltime_and_mainland(datalist,i,pars1,methode,reltolint,abstolint)
+      else
+        loglikelihood <- DAISIE_DE_logpEC_max_age_coltime_and_mainland(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 8) {
+      loglikelihood <- DAISIE_DE_logpNE_max_min_age_coltime(datalist,i,pars1,methode,reltolint,abstolint)
+    } else if (stac == 9) {
+      loglikelihood <- DAISIE_DE_logpES_max_min_age_coltime(datalist,i,pars1,methode,reltolint,abstolint)
+    } else {
+      stop("Unknown stac value: ", stac)
+    }
+
     vec_loglikelihood <- c(vec_loglikelihood, loglikelihood)
 
     print_parameters_and_loglik(
