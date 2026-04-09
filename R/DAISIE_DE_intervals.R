c38a94f67280730a6105a5a413f278602a2e7269
Ornela Dehayem <o.dehayem@gmail.com>
2026-04-09 18:49:53 +0200

    separate mu into mu_E and mu_NE

--- R/DAISIE_DE_intervals.R
+++ R/DAISIE_DE_intervals.R
@@ -1,7 +1,7 @@
 # pars1:
 # 1: lambda_c
-# 2: mu (endemic)
-# 3: mu2 (non-endemic)
+# 2: mu_E (endemic)
+# 3: mu_NE (non-endemic)
 # 4: gamma
 # 5: lambda_a
 
@@ -13,14 +13,15 @@
 interval3_NE <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
-    dDM1 <- -(lambdac + mu + lambdaa + gamma) * DM1 + (mu + lambdaa * E + lambdac * E * E) * DA2 + gamma * DM2
-    dDM2 <- -(lambdac + mu + lambdaa) * DM2 + (mu + lambdaa * E + lambdac * E * E) * DA2
+    dDM1 <- -(lambdac + mu_NE + lambdaa + gamma) * DM1 + (mu_NE + lambdaa * E + lambdac * E * E) * DA2 + gamma * DM2
+    dDM2 <- -(lambdac + mu_NE + lambdaa) * DM2 + (mu_NE + lambdaa * E + lambdac * E * E) * DA2
 
-    dE <- mu - (mu + lambdac) * E + lambdac * E * E
+    dE <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
     dDA2 <- -gamma * DA2 + gamma * DM2
 
     return(list(c(dDM1, dDM2, dE, dDA2)))
@@ -33,16 +34,17 @@ interval3_NE <- function(t, state, parameters) {
 # DAISIE_DE_logpEC_max_age_coltime
 # DAISIE_DE_logpEC
 #' @keywords internal
-interval2_EC <- function(t, state, parameters) {
-  with(as.list(c(state, parameters)), {
+interval2_EC <- function(t, state, pars1) {
+  with(as.list(c(state, pars1)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
-    dDE  <- -(lambdac + mu) * DE + 2 * lambdac * DE * E
-    dDM3 <- -(lambdac + mu + lambdaa) * DM3 + (mu + lambdaa * E + lambdac * E * E) * DA3
-    dE   <- mu - (mu + lambdac) * E + lambdac * E * E
+    dDE  <- -(lambdac + mu_E) * DE + 2 * lambdac * DE * E
+    dDM3 <- -(lambdac + mu_NE + lambdaa) * DM3 + (mu_NE + lambdaa * E + lambdac * E * E) * DA3
+    dE   <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
     dDA3 <- -gamma * DA3 + gamma * DM3
 
     return(list(c(dDE, dDM3, dE, dDA3)))
@@ -65,17 +67,18 @@ interval2_EC <- function(t, state, parameters) {
 # DAISIE_DE_logpNE_max_age_coltime
 # DAISIE_DE_logpNE_max_min_age_coltime
 #' @keywords internal
-interval4 <- function(t, state, parameters) {
-  with(as.list(c(state, parameters)), {
+interval4 <- function(t, state, pars1) {
+  with(as.list(c(state, pars1)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
     dDA1 <- -gamma * DA1 + gamma * DM1
 
-    dDM1 <- -(lambdac + mu + lambdaa) * DM1 + (mu + lambdaa * E + lambdac * E * E) * DA1
-    dE <- mu - (mu + lambdac) * E + lambdac * E * E
+    dDM1 <- -(lambdac + mu_NE + lambdaa) * DM1 + (mu_NE + lambdaa * E + lambdac * E * E) * DA1
+    dE <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
     return(list(c( dDA1, dDM1, dE)))
   })
 }
@@ -91,18 +94,19 @@ interval4 <- function(t, state, parameters) {
 interval3_ES <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
 
-    dDE <- -(lambdac + mu) * DE + 2 * lambdac * DE * E
+    dDE <- -(lambdac + mu_E) * DE + 2 * lambdac * DE * E
 
-    dDM1 <- -(lambdac + mu + lambdaa + gamma) * DM1 + gamma * DM2 + (mu + lambdaa * E + lambdac * E * E) * DA2
-    dDM2 <- -(lambdac + mu + lambdaa) * DM2 + (mu + lambdaa * E + lambdac * E * E) * DA2 + (lambdaa * DE + 2 * lambdac * DE * E) * DA3
-    dDM3 <- -(lambdac + mu + lambdaa) * DM3 + (mu + lambdaa * E + lambdac * E * E) * DA3
+    dDM1 <- -(lambdac + mu_NE + lambdaa + gamma) * DM1 + gamma * DM2 + (mu_NE + lambdaa * E + lambdac * E * E) * DA2
+    dDM2 <- -(lambdac + mu_NE + lambdaa) * DM2 + (mu_NE + lambdaa * E + lambdac * E * E) * DA2 + (lambdaa * DE + 2 * lambdac * DE * E) * DA3
+    dDM3 <- -(lambdac + mu_NE + lambdaa) * DM3 + (mu_NE + lambdaa * E + lambdac * E * E) * DA3
 
-    dE <- mu - (mu + lambdac) * E + lambdac * E * E
+    dE <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
 
     dDA2 <- -gamma * DA2 + gamma * DM2
     dDA3 <- -gamma * DA3 + gamma * DM3
@@ -117,17 +121,18 @@ interval3_ES <- function(t, state, parameters) {
 interval2_ES <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
-    dDE <- -(lambdac + mu) * DE + 2 * lambdac * DE * E
+    dDE <- -(lambdac + mu_E) * DE + 2 * lambdac * DE * E
 
-    dDM2 <- -(lambdac + mu + gamma + lambdaa) * DM2 + (lambdaa * DE + 2 * lambdac * DE * E) * DA3
+    dDM2 <- -(lambdac + mu_NE + gamma + lambdaa) * DM2 + (lambdaa * DE + 2 * lambdac * DE * E) * DA3
 
-    dDM3 <- -(lambdac + mu + lambdaa) * DM3 + (mu + lambdaa * E + lambdac * E * E) * DA3
+    dDM3 <- -(lambdac + mu_NE + lambdaa) * DM3 + (mu_NE + lambdaa * E + lambdac * E * E) * DA3
 
-    dE <- mu - (mu + lambdac) * E + lambdac * E * E
+    dE <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
 
     dDA3 <- -gamma * DA3 + gamma * DM3
 
@@ -145,14 +150,15 @@ interval2_ES <- function(t, state, parameters) {
 interval2_NE <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     lambdac <- parameters[1]
-    mu      <- parameters[2]
+    mu_E    <- parameters[2]
+    mu_NE   <- parameters[3]
     gamma   <- parameters[4]
     lambdaa <- parameters[5]
 
 
-    dDM2 <- -(lambdac + mu + gamma + lambdaa) * DM2
+    dDM2 <- -(lambdac + mu_NE + gamma + lambdaa) * DM2
 
-    dE <- mu - (mu + lambdac) * E + lambdac * E * E
+    dE <- mu_E - (mu_E + lambdac) * E + lambdac * E * E
 
     return(list(c(dDM2, dE)))
   })
