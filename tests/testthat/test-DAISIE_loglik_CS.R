b35a70581ccf73c94a489ae9e0f882288f82ed2b
rsetienne <rampaletienne@gmail.om>
2026-05-15 14:21:13 +0200

    Skip tests on cran

--- tests/testthat/test-DAISIE_loglik_CS.R
+++ tests/testthat/test-DAISIE_loglik_CS.R
@@ -1,6 +1,6 @@
 test_that("DAISIE_loglik_CS_choice produces correct output for CS_version 1", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   pars1 <- c(2.000, 2.700, 20.000, 0.009, 1.010)
   pars2 <- c(1.0e+02, 1.1e+01, 0.0e+00, 0.0e+00, NA)
   brts <- c(4.0000, 3.0282, 1.3227, 0.8223, 0.4286, 0.3462, 0.2450, 0.0808,
@@ -21,7 +21,7 @@ test_that("DAISIE_loglik_CS_choice produces correct output for CS_version 1", {
 test_that("DAISIE_loglik_CS_choice produces correct output for relaxed-rate
           model (CS_version = 2)", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   pars1 <- c(2.000, 2.700, 20.000, 0.009, 1.010)
   pars2 <- c(1.0e+02, 1.1e+01, 0.0e+00, 0.0e+00)
   brts <- c(4.0000, 3.0282, 1.3227, 0.8223, 0.4286, 0.3462, 0.2450, 0.0808,
@@ -46,7 +46,7 @@ test_that("DAISIE_loglik_CS_choice produces correct output for relaxed-rate
 
 test_that("DAISIE_loglik_CS_choice produces same output for CS_version = 0
           (with M = 1) and CS_version = 1 ", {
-  #skip_on_cran()
+  skip_on_cran()
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   pars1 <- c(2.000, 2.700, 20.000, 0.009, 1.010)
   pars2 <- c(100, 11, 0, 0)
@@ -76,7 +76,7 @@ test_that("DAISIE_loglik_CS_choice produces same output for CS_version = 0
 
 test_that("DAISIE_loglik_all produces correct output for relaxed-rate model", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   utils::data(Galapagos_datalist)
 
   invisible(capture.output(suppressWarnings(
@@ -185,7 +185,7 @@ test_that("DAISIE_loglik_all produces correct output for relaxed-rate model", {
 
 test_that("DAISIE_loglik produces correct output", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   output <- DAISIE_loglik(pars1 = c(2.061154e-09, 2.683455e+00, 1.000000e+01,
                                     9.332070e-03, 1.010073e+00),
                           pars2 = c(100, 0, 0, 0, NA),
@@ -203,7 +203,7 @@ test_that("DAISIE_loglik produces correct output", {
 test_that("DAISIE_loglik_all produces same output for CS_version 0 and 1 with
           and without conditioning", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   utils::data(Galapagos_datalist)
   Galapagos_datalist2 <- Galapagos_datalist
   for(i in 2:9) {
@@ -250,7 +250,7 @@ test_that("DAISIE_loglik_all produces same output for CS_version 0 and 1 with
 test_that("DAISIE_loglik_CS_choice produces equivalent output for ODEINT RKCK54
           and deSolve lsodes", {
   #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  #skip_on_cran()
+  skip_on_cran()
   pars1 <- c(2.000, 2.700, 20.000, 0.009, 1.010)
   pars2 <- c(1.0e+02, 1.1e+01, 0.0e+00, 0.0e+00, NA)
   brts <- c(4.0000, 3.0282, 1.3227, 0.8223, 0.4286, 0.3462, 0.2450, 0.0808,
